13eb216574f9bed756ce96b71a6db1736c577b7b
AlecThomson <alecthomson0210@gmail.com>
2019-10-16 10:39:21 +0800

    selavy files

--- cubes.py
+++ cubes.py
@@ -24,19 +24,19 @@ hdulist.close()
 
 island_idx = 10
 cutouts = []
-for i in tqdm(range(9)):
-    cutout = np.nanmean(data[:, \
-        islands['col_y_min'][i]:islands['col_y_max'][i], \
-            islands['col_x_min'][i]:islands['col_x_max'][i]]\
-                , axis=0)
-    cutouts.append(cutout)
+#for i in tqdm(range(9)):
+#    cutout = np.nanmean(data[:, \
+#        islands['col_y_min'][i]:islands['col_y_max'][i], \
+#            islands['col_x_min'][i]:islands['col_x_max'][i]]\
+#                , axis=0)
+#    cutouts.append(cutout)
 
 #pix = 100
 #cutout = np.nanmean(data[:, int(islands['col_y_cen'][island_idx])-pix:int(islands['col_y_cen'][island_idx])+pix, int(islands['col_x_cen'][island_idx])-pix:int(islands['col_x_cen'][island_idx])+pix], axis=0)
 
-fig, ax = plt.subplots(3, 3, sharex='col', sharey='row')
-for i in range(3):
-    for j in range(3):
-        ax[i, j].imshow(cutout[i+j])
-plt.savefig('test.png')
+#fig, ax = plt.subplots(3, 3, sharex='col', sharey='row')
+#for i in range(3):
+#    for j in range(3):
+#        ax[i, j].imshow(cutout[i+j])
+#plt.savefig('test.png')
 #hdu = 
