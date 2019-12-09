1c23310d07e005a6e550d59c24ed8bf627439daf
AlecThomson <alec.thomson@anu.edu.au>
2019-12-09 16:31:58 +1100

    Parallel farnes method

--- spiceracs/moments.py
+++ spiceracs/moments.py
@@ -20,14 +20,22 @@ class moments:
     """Methods for produce moment maps.
     """
 
-    def __init__(self, cube, basename, verbose=True):
+    def __init__(self, cube, basename, n_cores, verbose=True):
         self.cube = cube
         self.verbose = verbose
         self.basename = basename
+        self.n_cores = n_cores
 
     def mu(self, outdir='.', stoke=''):
         """Mean moment - freq axis first
         """
+        def mu_worker(queue, inarr, outarr):
+            while not queue.empty():
+                start, idx = queue.get()
+                yav = np.nanmean(np.array(self.cube[:, i, :]), axis=0)
+                yav[yav == 0] = np.nan
+                outarr[idx, :] = yav[:]
+
         try:
             os.mkdir(f'{outdir}/moments/')
             print('Made directory.')
@@ -41,21 +49,96 @@ class moments:
 
         if self.verbose:
             print(f'Writing to {momfile}...')
-        with fits.open(momfile, mode='update', memmap=True) as outfh:
-            for i in trange(
-                self.cube.shape[1],
-                desc='Looping over y-axis to save memory',
-                disable=(not self.verbose)
-            ):
-                yav = np.nanmean(np.array(self.cube[:, i, :]), axis=0)
-                yav[yav == 0] = np.nan
-                outfh[0].data[i, :] = yav
+        procs = []
+        width_max = 100
+        n_cores = cpu_to_use(self.n_cores, self.cube.shape[1])
+        width = self.cube.shape[1]//n_cores
+
+        if width > width_max:
+            width = cpu_to_use(width_max, self.cube.shape[1])
+
+        n_chunks = self.cube.shape[1]//width
+
+        outshape = list(self.cube.shape)
+        outshape[1] = width
+
+        # shared, can be used from multiple processes
+        mp_arr_out = mp.Array(c.c_double, int(np.prod(outshape)))
+        # then in each new process create a new numpy array using:
+        # mp_arr and arr share the same memory
+        buff_arr_out = np.frombuffer(mp_arr_out.get_obj())
+        # make it two-dimensional
+        # b and arr share the same memory
+        arr_out = buff_arr_out.reshape(outshape)
+        arr_out[:] = np.zeros(outshape)*np.nan
+
+        inshape = list(self.cube.shape)
+        inshape[1] = width
+
+        mp_arr_in = mp.Array(c.c_double, int(np.prod(inshape)))
+        # then in each new process create a new numpy array using:
+        # mp_arr and arr share the same memory
+        buff_arr_in = np.frombuffer(mp_arr_in.get_obj())
+        # make it two-dimensional
+        # b and arr share the same memory
+        arr_in = buff_arr_in.reshape(inshape)
+        arr_in[:] = np.zeros(inshape)*np.nan
+
+        q = mp.Queue()
+
+        tic = time.perf_counter()
+        for i in trange(
+                n_chunks, disable=(not self.verbose),
+                desc='Making mu in chunks'
+        ):
+            # if verbose:
+                #print('Beginning multiprocessing...')
+            procs = []
+            for j in range(n_cores):
+                proc = mp.Process(target=mu_worker, args=(
+                    q, arr_in, arr_out
+                )
+                )
+                procs.append(proc)
+
+            start = i*width
+            stop = start+width
+            # if verbose:
+            #print('Copying chunk of data to memory...')
+
+            dat = np.array(self.cube[:, start:stop, :])
+            dat[dat == 0] = np.nan
+            arr_in[:] = dat
+            for idx in range(stop-start):
+                q.put([start, idx])
+            # if verbose:
+                # print('Processing...')
+            for p in procs:
+                p.start()
+            for p in procs:
+                p.join()
+            # if verbose:
+                #print('Writing output to file...')
+            with fits.open(momfile, mode='update', memmap=True) as outfh:
+                outfh[0].data[start:stop] = arr_out[:]
                 outfh.flush()
+
+        toc = time.perf_counter()
+
+        if self.verbose:
+            print(f'Time taken was {toc - tic}s')
 
     def sigma(self, outdir='.', stoke=''):
         """
         Standard deviation moment - freq axis first
         """
+        def sigma_worker(queue, inarr, outarr):
+            while not queue.empty():
+                start, idx = queue.get()
+                ystd = np.nanstd(np.array(inarr[:, idx, :]), axis=0, ddof=1)
+                ystd[ystd == 0] = np.nan
+                outarr[idx, :] = ystd[:]
+
         try:
             os.mkdir(f'{outdir}/moments/')
             print('Made directory.')
@@ -70,19 +153,87 @@ class moments:
 
         if self.verbose:
             print(f'Writing to {momfile}...')
-        with fits.open(momfile, mode='update', memmap=True) as outfh:
-            for i in trange(
-                self.cube.shape[1],
-                desc='Looping over y-axis to save memory',
-                disable=(not self.verbose)
-            ):
-                ystd = np.nanstd(np.array(self.cube[:, i, :]), axis=0, ddof=1)
-                ystd[ystd == 0] = np.nan
-                outfh[0].data[i, :] = ystd
+        procs = []
+        width_max = 100
+        n_cores = cpu_to_use(self.n_cores, self.cube.shape[1])
+        width = self.cube.shape[1]//n_cores
+
+        if width > width_max:
+            width = cpu_to_use(width_max, self.cube.shape[1])
+
+        n_chunks = self.cube.shape[1]//width
+
+        outshape = list(self.cube.shape)
+        outshape[1] = width
+
+        # shared, can be used from multiple processes
+        mp_arr_out = mp.Array(c.c_double, int(np.prod(outshape)))
+        # then in each new process create a new numpy array using:
+        # mp_arr and arr share the same memory
+        buff_arr_out = np.frombuffer(mp_arr_out.get_obj())
+        # make it two-dimensional
+        # b and arr share the same memory
+        arr_out = buff_arr_out.reshape(outshape)
+        arr_out[:] = np.zeros(outshape)*np.nan
+
+        inshape = list(self.cube.shape)
+        inshape[1] = width
+
+        mp_arr_in = mp.Array(c.c_double, int(np.prod(inshape)))
+        # then in each new process create a new numpy array using:
+        # mp_arr and arr share the same memory
+        buff_arr_in = np.frombuffer(mp_arr_in.get_obj())
+        # make it two-dimensional
+        # b and arr share the same memory
+        arr_in = buff_arr_in.reshape(inshape)
+        arr_in[:] = np.zeros(inshape)*np.nan
+
+        q = mp.Queue()
+
+        tic = time.perf_counter()
+        for i in trange(
+                n_chunks, disable=(not self.verbose),
+                desc='Making sigma in chunks'
+        ):
+            # if verbose:
+                #print('Beginning multiprocessing...')
+            procs = []
+            for j in range(n_cores):
+                proc = mp.Process(target=sigma_worker, args=(
+                    q, arr_in, arr_out
+                )
+                )
+                procs.append(proc)
+
+            start = i*width
+            stop = start+width
+            # if verbose:
+            #print('Copying chunk of data to memory...')
+
+            dat = np.array(self.cube[:, start:stop, :])
+            dat[dat == 0] = np.nan
+            arr_in[:] = dat
+            for idx in range(stop-start):
+                q.put([start, idx])
+            # if verbose:
+                # print('Processing...')
+            for p in procs:
+                p.start()
+            for p in procs:
+                p.join()
+            # if verbose:
+                #print('Writing output to file...')
+            with fits.open(momfile, mode='update', memmap=True) as outfh:
+                outfh[0].data[start:stop] = arr_out[:]
                 outfh.flush()
 
+        toc = time.perf_counter()
 
-def momentloop(datadict, outdir='.', verbose=True):
+        if self.verbose:
+            print(f'Time taken was {toc - tic}s')
+
+
+def momentloop(datadict, n_cores, outdir='.', verbose=True):
     """Produce moment maps.
 
     Args:
@@ -99,7 +250,8 @@ def momentloop(datadict, outdir='.', verbose=True):
     for stokes in ['p', 'q', 'u']:
         if verbose:
             print(f'Making moments for Stokes {stokes}...')
-        mom = moments(datadict[f'{stokes}_cube'], basename, verbose=verbose)
+        mom = moments(datadict[f'{stokes}_cube'],
+                      basename, n_cores, verbose=verbose)
         if verbose:
             print(f'Making mean...')
         mom.mu(outdir=outdir, stoke=stokes)
@@ -143,114 +295,123 @@ def makepi(datadict, n_cores, outdir='.', verbose=True):
 
     pifilename = os.path.basename(datadict['i_file'].replace('.i.', '.p.'))
     pifile = f'{outdir}/moments/{pifilename}'
-    if verbose:
-        print(f'Writing to {pifile}...')
+
     try:
-        with open(pifile, 'rb') as fh:
-            pass
+        p_cube = SpectralCube.read(pifile, mode='denywrite')
+        datadict['p_file'] = pifile
+        datadict['p_cube'] = p_cube
+        return datadict
+
     except FileNotFoundError:
-        copyfile(datadict['i_file'], pifile, verbose=verbose)
+        if verbose:
+            print(f'Writing to {pifile}...')
+        try:
+            with open(pifile, 'rb') as fh:
+                pass
+        except FileNotFoundError:
+            copyfile(datadict['i_file'], pifile, verbose=verbose)
 
-    if verbose:
-        print('Initialising arrays...')
-        print('Note: Output data will be held in memory.')
+        if verbose:
+            print('Initialising arrays...')
+            print('Note: Output data will be held in memory.')
 
-    procs = []
-    width_max = 100
-    n_cores = cpu_to_use(n_cores, datadict['i_cube'].shape[1])
-    width = datadict['i_cube'].shape[1]//n_cores
-
-    if width > width_max:
-        width = cpu_to_use(width_max, datadict['i_cube'].shape[1])
-
-    n_chunks = datadict['i_cube'].shape[1]//width
-
-    outshape = list(datadict['i_cube'].shape)
-    outshape.insert(1, 1)  # For Stokes axis
-    outshape[2] = width
-
-    # shared, can be used from multiple processes
-    mp_arr_out = mp.Array(c.c_double, int(np.prod(outshape)))
-    # then in each new process create a new numpy array using:
-    # mp_arr and arr share the same memory
-    buff_arr_out = np.frombuffer(mp_arr_out.get_obj())
-    # make it two-dimensional
-    arr_out = buff_arr_out.reshape(outshape)  # b and arr share the same memory
-    arr_out[:] = np.zeros(outshape)*np.nan
-
-    inshape = list(datadict['i_cube'].shape)
-    inshape[1] = width
-
-    mp_arr_in_q = mp.Array(c.c_double, int(np.prod(inshape)))
-    # then in each new process create a new numpy array using:
-    # mp_arr and arr share the same memory
-    buff_arr_in_q = np.frombuffer(mp_arr_in_q.get_obj())
-    # make it two-dimensional
-    # b and arr share the same memory
-    arr_in_q = buff_arr_in_q.reshape(inshape)
-    arr_in_q[:] = np.zeros(inshape)*np.nan
-
-    mp_arr_in_u = mp.Array(c.c_double, int(np.prod(inshape)))
-    # then in each new process create a new numpy array using:
-    # mp_arr and arr share the same memory
-    buff_arr_in_u = np.frombuffer(mp_arr_in_u.get_obj())
-    # make it two-dimensional
-    # b and arr share the same memory
-    arr_in_u = buff_arr_in_u.reshape(inshape)
-    arr_in_u[:] = np.zeros(inshape)*np.nan
-
-    q = mp.Queue()
-
-    tic = time.perf_counter()
-    for i in trange(
-            n_chunks, disable=(not verbose), desc='Making PI cube in chunks'
-    ):
-        #if verbose:
-            #print('Beginning multiprocessing...')
         procs = []
-        for j in range(n_cores):
-            proc = mp.Process(target=makepi_worker, args=(
-                q, arr_in_q, arr_in_u, arr_out, verbose
-            )
-            )
-            procs.append(proc)
+        width_max = 100
+        n_cores = cpu_to_use(n_cores, datadict['i_cube'].shape[1])
+        width = datadict['i_cube'].shape[1]//n_cores
 
-        start = i*width
-        stop = start+width
-        #if verbose:
+        if width > width_max:
+            width = cpu_to_use(width_max, datadict['i_cube'].shape[1])
+
+        n_chunks = datadict['i_cube'].shape[1]//width
+
+        outshape = list(datadict['i_cube'].shape)
+        outshape.insert(1, 1)  # For Stokes axis
+        outshape[2] = width
+
+        # shared, can be used from multiple processes
+        mp_arr_out = mp.Array(c.c_double, int(np.prod(outshape)))
+        # then in each new process create a new numpy array using:
+        # mp_arr and arr share the same memory
+        buff_arr_out = np.frombuffer(mp_arr_out.get_obj())
+        # make it two-dimensional
+        # b and arr share the same memory
+        arr_out = buff_arr_out.reshape(outshape)
+        arr_out[:] = np.zeros(outshape)*np.nan
+
+        inshape = list(datadict['i_cube'].shape)
+        inshape[1] = width
+
+        mp_arr_in_q = mp.Array(c.c_double, int(np.prod(inshape)))
+        # then in each new process create a new numpy array using:
+        # mp_arr and arr share the same memory
+        buff_arr_in_q = np.frombuffer(mp_arr_in_q.get_obj())
+        # make it two-dimensional
+        # b and arr share the same memory
+        arr_in_q = buff_arr_in_q.reshape(inshape)
+        arr_in_q[:] = np.zeros(inshape)*np.nan
+
+        mp_arr_in_u = mp.Array(c.c_double, int(np.prod(inshape)))
+        # then in each new process create a new numpy array using:
+        # mp_arr and arr share the same memory
+        buff_arr_in_u = np.frombuffer(mp_arr_in_u.get_obj())
+        # make it two-dimensional
+        # b and arr share the same memory
+        arr_in_u = buff_arr_in_u.reshape(inshape)
+        arr_in_u[:] = np.zeros(inshape)*np.nan
+
+        q = mp.Queue()
+
+        tic = time.perf_counter()
+        for i in trange(
+                n_chunks, disable=(not verbose), desc='Making PI cube in chunks'
+        ):
+            # if verbose:
+                #print('Beginning multiprocessing...')
+            procs = []
+            for j in range(n_cores):
+                proc = mp.Process(target=makepi_worker, args=(
+                    q, arr_in_q, arr_in_u, arr_out, verbose
+                )
+                )
+                procs.append(proc)
+
+            start = i*width
+            stop = start+width
+            # if verbose:
             #print('Copying chunk of data to memory...')
 
-        datq = np.array(datadict['q_cube'][:, start:stop, :])
-        datu = np.array(datadict['u_cube'][:, start:stop, :])
-        datq[datq == 0] = np.nan
-        datu[datu == 0] = np.nan
-        arr_in_q[:] = datq
-        arr_in_u[:] = datu
-        for idx in range(stop-start):
-            q.put([start, idx])
-        #if verbose:
-            #print('Processing...')
-        for p in procs:
-            p.start()
-        for p in procs:
-            p.join()
-        #if verbose:
-            #print('Writing output to file...')
-        with fits.open(pifile, mode='update', memmap=True) as outfh:
-            outfh[0].data[:, :, start:stop] = arr_out[:]
-            outfh.flush()
+            datq = np.array(datadict['q_cube'][:, start:stop, :])
+            datu = np.array(datadict['u_cube'][:, start:stop, :])
+            datq[datq == 0] = np.nan
+            datu[datu == 0] = np.nan
+            arr_in_q[:] = datq
+            arr_in_u[:] = datu
+            for idx in range(stop-start):
+                q.put([start, idx])
+            # if verbose:
+                # print('Processing...')
+            for p in procs:
+                p.start()
+            for p in procs:
+                p.join()
+            # if verbose:
+                #print('Writing output to file...')
+            with fits.open(pifile, mode='update', memmap=True) as outfh:
+                outfh[0].data[:, :, start:stop] = arr_out[:]
+                outfh.flush()
 
-    toc = time.perf_counter()
+        toc = time.perf_counter()
 
-    if verbose:
-        print('PI cube done!')
-        print(f'Time taken was {toc - tic}s')
+        if verbose:
+            print('PI cube done!')
+            print(f'Time taken was {toc - tic}s')
 
-    p_cube = SpectralCube.read(pifile, mode='denywrite')
-    datadict['p_file'] = pifile
-    datadict['p_cube'] = p_cube
+        p_cube = SpectralCube.read(pifile, mode='denywrite')
+        datadict['p_file'] = pifile
+        datadict['p_cube'] = p_cube
 
-    return datadict
+        return datadict
 
 
 def makezero_worker(queue, inarr_q, inarr_u, inarr_f, outarr, verbose):
@@ -427,7 +588,7 @@ def main(args, verbose=True):
     if args.farnes:
         if verbose:
             print('Making moment maps...')
-        momentloop(datadict, outdir=outdir, verbose=verbose)
+        momentloop(datadict, n_cores, outdir=outdir, verbose=verbose)
 
     if args.zero:
         if verbose:
