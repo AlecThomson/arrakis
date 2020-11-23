84f31746f92013fcc4e1b207c5bed906c84977d6
Thomson, Alec (CASS, Kensington) <Alec.Thomson@csiro.au>
2020-11-23 15:21:16 +0800

    Make a linmos pipeline

--- spiceracs/linmos.py
+++ spiceracs/linmos.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env /group/askap/athomson/miniconda3/envs/aces/bin/python
 
 import os
 import subprocess
@@ -11,7 +11,141 @@ from astropy.coordinates import SkyCoord
 import astropy.units as u
 from tqdm import tqdm, trange
 from IPython import embed
-from spiceracs.utils import gen_seps
+from aces.obsplan.config import ACESConfig  # noqa
+from astropy.coordinates import SkyCoord
+import astropy
+
+
+def gen_pipline(slurm_dir, fieldname, mongobat, slurmbats, dryrun=True):
+    """Generate linmos pipeline
+
+    Args:
+        slurm_dir (str): directory for slurm files
+        fieldname (str): Name of RACS field
+        mongobat (str): Name of mongo batch file
+        slurmbats (list): List of linmos batch files
+        dryrun (bool, optional): Do a dryrun. Defaults to True.
+    """
+    pipeline = f"""#! /bin/bash
+# first job - no dependencies
+RES=$(sbatch {mongobat})
+JID=$(echo $RES | cut -d " " -f 4)
+"""
+
+    for slurmbat in slurmbats:
+        pipeline += f"""
+sbatch  --dependency=after:$JID {slurmbat}
+    """
+    pipefile = f"{slurm_dir}/{fieldname}_linmos_pipeline.sh"
+    with open(pipefile, "w") as f:
+        f.write(pipeline)
+    print(f"Written pipeline file to {pipefile}")
+    if not dryrun:
+        print(f"Submitting {pipefile}")
+        command = f"sh {pipefile}"
+        command = shlex.split(command)
+        proc = subprocess.run(
+            command, encoding="utf-8", check=True
+        )
+
+
+def gen_mongo(fieldname, slurm_dir, logdir):
+    """Generate mongodb sbatch
+
+    Args:
+        fieldname (str): field name
+        slurm_dir (str): directory
+        logdir (str): directory
+    """
+    script = f"""#!/bin/bash -l
+#SBATCH --partition=workq
+#SBATCH --clusters=galaxy
+#SBATCH --exclude=nid00[010,200-202]
+#SBATCH --account=askap
+# No further constraints applied
+# No reservation requested
+#SBATCH --mail-user=alec.thomson@csiro.au
+#SBATCH --mail-type=ALL
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=1
+#SBATCH --job-name=mongo_test_linmos
+#SBATCH --export=NONE
+
+cd /group/askap/athomson/repos/spiceracs
+log={logdir}/{fieldname}_${{SLURM_JOB_ID}}_mongo.log
+conda activate py36
+numactl --interleave=all mongod --dbpath=database --bind_ip $(hostname -i) --fork --logpath $log
+echo $(hostname -i) > {logdir}/mongo_ip.txt
+while true; do :; done
+wait
+"""
+    mongobat = f"{slurm_dir}/{fieldname}_mongo.sbatch"
+    with open(mongobat, "w") as f:
+        f.write(script)
+    print(f"Written slurm file to {mongobat}")
+    return mongobat
+
+
+def gen_seps(field):
+    """Get beam separations
+
+    Args:
+        field (str): File name e.g. 2132-50A
+
+    Returns:
+        Table: Separation table
+    """
+    # get offsets
+    aces_cfg = ACESConfig()
+    fp_factory = aces_cfg.footprint_factory
+    # fp = fp_factory.make_footprint('ak:closepack36', 0.9 * pi / 180., 0.0 * pi / 180.)
+    fp = fp_factory.make_footprint(
+        'ak:square_6x6', 1.05 * np.pi / 180., 0.0 * np.pi / 180.)
+
+    offsets = np.rad2deg(np.array(fp.offsetsRect))
+
+    master_cat = Table.read('askap_surveys/RACS/admin/epoch_0/field_data.csv')
+    master_cat.add_index('FIELD_NAME')
+    master_cat = master_cat.loc[f"RACS_test4_1.05_{field}"]
+    if type(master_cat) is not astropy.table.row.Row:
+        master_cat = master_cat[0]
+
+    # Look for multiple SBIDs - only need one
+    cats = glob(
+        f'askap_surveys/RACS/admin/epoch_0/beam_inf_*-RACS_test4_1.05_{field}.csv')
+    beam_cat = Table.read(cats[0])
+    beam_cat.add_index('BEAM_NUM')
+
+    names = ["BEAM", "DELTA_RA", "DELTA_DEC", "BEAM_RA",
+             "BEAM_DEC", "FOOTPRINT_RA", "FOOTPRINT_DEC"]
+
+    cols = []
+    for beam in range(36):
+        beam = int(beam)
+
+        beam_dat = beam_cat.loc[beam]
+        beam_coord = SkyCoord(
+            beam_dat['RA_DEG']*u.deg, beam_dat['DEC_DEG']*u.deg)
+        field_coord = SkyCoord(
+            master_cat['RA_DEG']*u.deg, master_cat['DEC_DEG']*u.deg)
+        ra_beam = beam_coord.ra.hms
+        dec_beam = beam_coord.dec.dms
+        ra_field = field_coord.ra.hms
+        dec_field = field_coord.dec.dms
+
+        row = [
+            beam,
+            f"{offsets[beam][0]:0.3f}",
+            f"{offsets[beam][1]:0.3f}",
+            f"{ra_beam.h:02.0f}:{ra_beam.m:02.0f}:{ra_beam.s:06.3f}",
+            f"{dec_beam.d:02.0f}:{abs(dec_beam.m):02.0f}:{abs(dec_beam.s):05.2f}",
+            f"{ra_field.h:02.0f}:{ra_field.m:02.0f}:{ra_field.s:06.3f}",
+            f"{dec_field.d:02.0f}:{abs(dec_field.m):02.0f}:{abs(dec_field.s):05.2f}",
+        ]
+        cols += [row]
+    tab = Table(data=np.array(cols), names=names)
+
+    return tab
 
 
 def genparset(field, stoke, datadir, septab, prefix=""):
@@ -26,15 +160,15 @@ def genparset(field, stoke, datadir, septab, prefix=""):
 
     Raises:
         Exception: If no files are found in the datadir
-    """    
+    """
     ims = sorted(
         glob(
-            f"{datadir}/sm.*.cutout.image.restored.{stoke.lower()}.*.beam*.fits"
+            f"{datadir}/sm.*.cutout.image.restored.{stoke.lower()}.*.beam*[00-35.fits]"
         )
     )
     if len(ims) == 0:
         print(
-            f"{datadir}/sm.*.cutout.image.restored.{stoke.lower()}.*.beam*.fits"
+            f"{datadir}/sm.*.cutout.image.restored.{stoke.lower()}.*.beam*[00-35.fits]"
         )
         raise Exception(
             'No files found. Have you run imaging? Check your prefix?')
@@ -45,7 +179,7 @@ def genparset(field, stoke, datadir, septab, prefix=""):
 
     wgts = sorted(
         glob(
-            f"{datadir}/*.weights.{stoke.lower()}.*.beam*.fits"
+            f"{datadir}/*.weights.{stoke.lower()}.*.beam*[00-35.fits]"
         )
     )
     weightlist = "["
@@ -71,9 +205,9 @@ linmos.feeds.spacing    = 1deg
     for im in ims:
         basename = os.path.basename(im).replace('.fits', '')
         idx = basename.find('beam')
-        beamno = int(basename[len('beam')+idx:])
+        beamno = int(basename[len('beam')+idx:len('beam')+idx+2])
         idx = septab['BEAM'] == beamno
-        offset = f"linmos.feeds.{basename} = [{septab[idx]['DELTA_RA'][0]},{septab[idx]['DELTA_DEC'][0]}]\n"
+        offset = f"linmos.feeds.{basename} = [{septab[idx]['DELTA_RA']},{septab[idx]['DELTA_DEC']}]\n"
         parset += offset
     with open(parset_file, "w") as f:
         f.write(parset)
@@ -90,7 +224,7 @@ def genslurm(parsets, fieldname, cutdir, files, stokeslist, dryrun=True):
         cutdir (str): Directory containing cutouts
         files (list): List of directories inside cutouts
         dryrun (bool, optional): Generate SBATCH but do not submit. Defaults to True.
-    """    
+    """
     if dryrun:
         print("Doing a dryrun - no jobs will be submitted")
     slurm_dir = f"{cutdir}/slurmFiles"
@@ -104,6 +238,7 @@ def genslurm(parsets, fieldname, cutdir, files, stokeslist, dryrun=True):
     except FileExistsError:
         pass
 
+    slurmbats = []
     for stoke in stokeslist:
         slurmbat = f"{slurm_dir}/{fieldname}_linmos_{stoke}.sbatch"
         slurm = f"""#!/bin/bash -l
@@ -116,7 +251,7 @@ def genslurm(parsets, fieldname, cutdir, files, stokeslist, dryrun=True):
 #SBATCH --mail-user=alec.thomson@csiro.au
 #SBATCH --mail-type=ALL
 #SBATCH --time=24:00:00
-#SBATCH --ntasks=288
+#SBATCH --ntasks=20
 #SBATCH --ntasks-per-node=20
 #SBATCH --job-name={stoke}_{fieldname}_linmos
 #SBATCH --export=NONE
@@ -137,16 +272,21 @@ module load numpy
 module load matplotlib
 module load astropy
 #module load askapsoft
+# MW experimental LINMOS
+module unload askapsoft
+module use /group/askap/wie017/modulefiles
+module load askapsoft/dev-omp
 # Fixed linmos
-module load askapsoft/66f1e70
+#module load askapsoft/66f1e70
 # Exit if we could not load askapsoft
-if [ "$ASKAPSOFT_RELEASE" == "" ]; then
-    echo "ERROR: \$ASKAPSOFT_RELEASE not available - could not load askapsoft module."
-    exit 1
-fi
+#if [ "$ASKAPSOFT_RELEASE" == "" ]; then
+#    echo "ERROR: \$ASKAPSOFT_RELEASE not available - could not load askapsoft module."
+#    exit 1
+#fi
 
-NCORES=288
+NCORES=20
 NPPN=20
+mongo_ip=$(cat {logdir}/mongo_ip.txt)
 
 """
         # Get island dirs
@@ -168,20 +308,18 @@ islandList="{island_list}"
 for island in $islandList; do
     dir={cutdir}/${{island}}
     cd $dir
-    srun --export=ALL --ntasks=$NCORES --ntasks-per-node=$NPPN linmos-mpi -c ${{dir}}/linmos_{stoke}.in >> "$log"
-done | tqdm --total {len(islands)} 2>&1
+    #linmos -c ${{dir}}/linmos_{stoke}.in >> "$log"
+    srun --export=ALL --ntasks=${{NCORES}} --ntasks-per-node=${{NPPN}} linmos-mpi -c ${{dir}}/linmos_{stoke}.in > "$log"
+    ls sm*image.restored.{stoke.lower()}*.linmos.fits | xargs -I // mongo --host $mongo_ip --eval 'db.islands.findOneAndUpdate({{"island_id" : "'$island'"}}, {{"$set" :{{"{stoke.lower()}_file" : "'//'"}}}});' spiceracs
+    echo ${{island}}
+done | tqdm --total {len(islands)} >> /dev/null
 """
         slurm += cmd
         with open(slurmbat, "w") as f:
             f.write(slurm)
         print(f"Written slurm file to {slurmbat}")
-        if not dryrun:
-            print(f"Submitting {slurmbat}")
-            command = f"sbatch {slurmbat}"
-            command = shlex.split(command)
-            proc = subprocess.run(
-                command, encoding="utf-8", check=True
-            )
+        slurmbats += [slurmbat]
+    return slurmbats, slurm_dir, logdir
 
 
 def yes_or_no(question):
@@ -192,7 +330,7 @@ def yes_or_no(question):
 
     Returns:
         bool: True for yes, False for no
-    """    
+    """
     while "Please answer 'y' or 'n'":
         reply = str(input(question+' (y/n): ')).lower().strip()
         if reply[:1] == 'y':
@@ -237,10 +375,17 @@ def main(args):
             parfile = genparset(field, stoke.capitalize(),
                                 file, beamseps, prefix=args.prefix)
             parfiles.append(parfile)
-    
-    genslurm(parfiles, field, cutdir, files, stokeslist, dryrun=dryrun)
+
+    slurmbats, slurm_dir, logdir = genslurm(parfiles,
+                                            field,
+                                            cutdir,
+                                            files,
+                                            stokeslist)
+    mongobat = gen_mongo(field, slurm_dir, logdir)
+    gen_pipline(slurm_dir, field, mongobat, slurmbats, dryrun=dryrun)
 
     print('Done!')
+
 
 def cli():
     """Command-line interface
