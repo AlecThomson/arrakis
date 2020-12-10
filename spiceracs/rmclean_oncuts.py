e3ec51fc6071d27c5eff836e5a25f3bc9b5eced7
Thomson, Alec (CASS, Kensington) <Alec.Thomson@csiro.au>
2020-12-10 13:37:27 +0800

    CLEAN is fixed

--- spiceracs/rmclean_oncuts.py
+++ spiceracs/rmclean_oncuts.py
@@ -17,145 +17,104 @@ import matplotlib.pyplot as plt
 from RMutils.util_misc import create_frac_spectra
 import functools
 import psutil
-print = functools.partial(print,f'[{psutil.Process().cpu_num()}]', flush=True)
+print = functools.partial(print, f'[{psutil.Process().cpu_num()}]', flush=True)
 
 
 def rmclean1d(args):
-    i, clargs, outdir, verbose = args
+    comp_id, clargs, outdir, host, field, verbose = args
 
-    client = pymongo.MongoClient()  # default connection (ie, local)
-    mydb = client['racs']  # Create/open database
-    mycol = mydb['spice']  # Create/open collection
+    with pymongo.MongoClient(host=host) as client:
+        mydb = client['spiceracs']  # Create/open database
+        isl_col = mydb['islands']  # Create/open collection
+        comp_col = mydb['components']  # Create/open collection
+        beams_col = mydb['beams']  # Create/open collection
 
     # Basic querey
-    if clargs.pol and not clargs.unres:
-        myquery = {"polarized": True}
-        myquery = {"$and": [{"rmsynth1d": True}, myquery]}
-    elif clargs.unres and not clargs.pol:
-        myquery = {"resolved": False}
-        myquery = {"$and": [{"rmsynth1d": True}, myquery]}
-    elif clargs.pol and clargs.unres:
-        myquery = {"$and": [{"rmsynth1d": True}, {
-            "resolved": False}, {"polarized": True}]}
+    myquery = {"Component_ID": comp_id}
 
-    elif clargs.pol and not clargs.loners:
-        myquery = {"polarized": True}
-        myquery = {"$and": [{"rmsynth1d": True}, myquery]}
-    elif clargs.loners and not clargs.pol:
-        myquery = {"n_components": 1}
-        myquery = {"$and": [{"rmsynth1d": True}, myquery]}
-    elif clargs.pol and clargs.loners:
-        myquery = {"$and": [{"rmsynth1d": True}, {
-            "n_components": 1}, {"polarized": True}]}
+    doc = comp_col.find_one(myquery)
 
-    else:
-        myquery = {"rmsynth1d": True}
+    iname = doc['Source_ID']
+    beam = beams_col.find_one({'Source_ID': iname})
 
-    doc = mycol.find(myquery).sort("flux_peak", -1)
+    if clargs.rm_verbose:
+        print(f'Working on {comp_id}')
+    try:
+        cname = comp_id
+        ifile = beam['beams'][field]['i_file']
+        outdir = f"{os.path.dirname(ifile)}"
+        prefix = f"{outdir}/{cname}"
 
+        rm1dfiles = doc['rm1dfiles']
+        fdfFile = f"{outdir}/{rm1dfiles['FDF_dirty']}"
+        rmsfFile = f"{outdir}/{rm1dfiles['RMSF']}"
+        weightFile = f"{outdir}/{rm1dfiles['weights']}"
+        rmSynthFile = f"{outdir}/{rm1dfiles['summary_json']}"
+        # Sanity checks
+        for f in [weightFile, fdfFile, rmsfFile, rmSynthFile]:
+            if not os.path.exists(f):
+                print("File does not exist: '{:}'.".format(f), end=' ')
+                sys.exit()
+        nBits = 32
+        mDictS, aDict = do_RMclean_1D.readFiles(
+            fdfFile, rmsfFile, weightFile, rmSynthFile, nBits)
+        # Run RM-CLEAN on the spectrum
+        outdict, arrdict = do_RMclean_1D.run_rmclean(mDictS=mDictS,
+                                                     aDict=aDict,
+                                                     cutoff=clargs.cutoff,
+                                                     maxIter=clargs.maxIter,
+                                                     gain=clargs.gain,
+                                                     nBits=nBits,
+                                                     showPlots=clargs.showPlots,
+                                                     verbose=clargs.rm_verbose)
 
-    iname = doc[i]['island_name']
-    for comp in range(int(doc[i]['n_components'])):
-        if clargs.rm_verbose:
-            print(f'Working on component {comp+1}')
-        try:
-            if doc[i][f"comp_{comp+1}_rmsynth1d"] is not True:
-                return
-            else:
-                cname = doc[i][f'component_{comp+1}']['component_name']
-                prefix = f'{outdir}/{cname}'
+        # Save output
+        do_RMclean_1D.saveOutput(outdict,
+                                 arrdict,
+                                 prefixOut=prefix,
+                                 verbose=clargs.rm_verbose)
 
-                fdfFile = prefix + "_FDFdirty.dat"
-                rmsfFile = prefix + "_RMSF.dat"
-                weightFile = prefix + "_weight.dat"
-                rmSynthFile = prefix + "_RMsynth.json"
-                # Sanity checks
-                for f in [weightFile, fdfFile, rmsfFile, rmSynthFile]:
-                    if not os.path.exists(f):
-                        print("File does not exist: '{:}'.".format(f), end=' ')
-                        sys.exit()
-                nBits = 32
-                mDictS, aDict = do_RMclean_1D.readFiles(
-                    fdfFile, rmsfFile, weightFile, rmSynthFile, nBits)
-                # Run RM-CLEAN on the spectrum
-                outdict, arrdict = do_RMclean_1D.run_rmclean(mDictS=mDictS,
-                                                            aDict=aDict,
-                                                            cutoff=clargs.cutoff,
-                                                            maxIter=clargs.maxIter,
-                                                            gain=clargs.gain,
-                                                            nBits=nBits,
-                                                            showPlots=clargs.showPlots,
-                                                            verbose=clargs.rm_verbose)
+        if clargs.database:
+            # Load into Mongo
+            myquery = {"Component_ID": cname}
 
-                # Save output
-                do_RMclean_1D.saveOutput(outdict,
-                                        arrdict,
-                                        prefixOut=prefix,
-                                        verbose=clargs.rm_verbose)
+            newvalues = {"$set": {f"rmclean1d": True}}
+            comp_col.update_one(myquery, newvalues)
 
-                if clargs.database:
-                    # Load into Mongo
-                    myquery = {"island_name": iname}
+            newvalues = {"$set": {f"rm_summary": outdict}}
+            comp_col.update_one(myquery, newvalues)
 
-                    newvalues = {"$set": {f"comp_{comp+1}_rmclean1d": True}}
-                    mycol.update_one(myquery, newvalues)
-
-                    newvalues = {"$set": {f"comp_{comp+1}_rm_summary": outdict}}
-                    mycol.update_one(myquery, newvalues)
-        except KeyError:
-            print('Failed to load data! RM-CLEAN not applied to component!')
-            print(f'Island is {iname}, component number is {comp+1}')
-            return
-            
-    if clargs.database:
-        # Load into Mongo
-        myquery = {"island_name": iname}
-
-        newvalues = {"$set": {f"rmclean1d": True}}
-        mycol.update_one(myquery, newvalues)
+    except KeyError:
+        print('Failed to load data! RM-CLEAN not applied to component!')
+        print(f'Island is {iname}, component is {cname}')
+        return
 
 
 def rmclean3d(args):
-    i, clargs, outdir, verbose = args
+    island_id, clargs, outdir, host, field, verbose = args
 
-    client = pymongo.MongoClient()  # default connection (ie, local)
-    mydb = client['racs']  # Create/open database
-    mycol = mydb['spice']  # Create/open collection
+    with pymongo.MongoClient(host=host) as client:
+        mydb = client['spiceracs']  # Create/open database
+        isl_col = mydb['islands']  # Create/open collection
+        comp_col = mydb['components']  # Create/open collection
+        beams_col = mydb['beams']  # Create/open collection
 
     # Basic querey
-    if clargs.pol and not clargs.unres:
-        myquery = {"polarized": True}
-        myquery = {"$and": [{"rmsynth1d": True}, myquery]}
-    elif clargs.unres and not clargs.pol:
-        myquery = {"resolved": False}
-        myquery = {"$and": [{"rmsynth1d": True}, myquery]}
-    elif clargs.pol and clargs.unres:
-        myquery = {"$and": [{"rmsynth1d": True}, {
-            "resolved": False}, {"polarized": True}]}
+    myquery = {"Source_ID": island_id}
 
-    elif clargs.pol and not clargs.loners:
-        myquery = {"polarized": True}
-        myquery = {"$and": [{"rmsynth1d": True}, myquery]}
-    elif clargs.loners and not clargs.pol:
-        myquery = {"n_components": 1}
-        myquery = {"$and": [{"rmsynth1d": True}, myquery]}
-    elif clargs.pol and clargs.loners:
-        myquery = {"$and": [{"rmsynth1d": True}, {
-            "n_components": 1}, {"polarized": True}]}
+    doc = comp_col.find_one(myquery)
+    iname = doc['Source_ID']
+    prefix = f"{iname}_"
+    beam = beams_col.find_one({'Source_ID': iname})
+    ifile = beam['beams'][field]['i_file']
+    outdir = os.path.dirname(ifile)
 
-    else:
-        myquery = {"rmsynth1d": True}
-
-    doc = mycol.find(myquery).sort("flux_peak", -1)
-
-    iname = doc[i]['island_name']
-    qfile = f"{outdir}/{doc[i]['q_file']}"
-    ufile = f"{outdir}/{doc[i]['u_file']}"
-    vfile = f"{outdir}/{doc[i]['v_file']}"
+    island = isl_col.find_one(myquery)
+    rm3dfiles = island["rm3dfiles"]
 
     cleanFDF, ccArr, iterCountArr, residFDF, headtemp = do_RMclean_3D.run_rmclean(
-        fitsFDF=f"{outdir}/{iname}FDF_real_dirty.fits",
-        fitsRMSF=f"{outdir}/{iname}RMSF_tot.fits",
+        fitsFDF=f"{outdir}/{rm3dfiles['FDF_real_dirty']}",
+        fitsRMSF=f"{outdir}/{rm3dfiles['RMSF_tot']}",
         cutoff=clargs.cutoff,
         maxIter=clargs.maxIter,
         gain=clargs.gain,
@@ -170,7 +129,7 @@ def rmclean3d(args):
         iterCountArr,
         residFDF,
         headtemp,
-        prefixOut=iname,
+        prefixOut=prefix,
         outDir=outdir,
         write_separate_FDF=True,
         verbose=clargs.rm_verbose)
@@ -181,46 +140,68 @@ def main(pool, args, verbose=False):
     if outdir[-1] == '/':
         outdir = outdir[:-1]
     outdir = f'{outdir}/cutouts'
-    client = pymongo.MongoClient()  # default connection (ie, local)
-    mydb = client['racs']  # Create/open database
-    mycol = mydb['spice']  # Create/open collection
+    field = args.field
 
-    # Basic querey
-    if args.pol and not args.unres:
-        myquery = {"polarized": True}
-        myquery = {"$and": [{"rmsynth1d": True}, myquery]}
-    elif args.unres and not args.pol:
-        myquery = {"resolved": False}
-        myquery = {"$and": [{"rmsynth1d": True}, myquery]}
-    elif args.pol and args.unres:
-        myquery = {"$and": [{"rmsynth1d": True}, {
-            "resolved": False}, {"polarized": True}]}
+    host = args.host
+    # default connection (ie, local)
+    with pymongo.MongoClient(host=host) as client:
+        mydb = client['spiceracs']  # Create/open database
+        isl_col = mydb['islands']  # Create/open collection
+        comp_col = mydb['components']  # Create/open collection
+        beams_col = mydb['beams']  # Create/open collection
 
-    elif args.pol and not args.loners:
-        myquery = {"polarized": True}
-        myquery = {"$and": [{"rmsynth1d": True}, myquery]}
-    elif args.loners and not args.pol:
-        myquery = {"n_components": 1}
-        myquery = {"$and": [{"rmsynth1d": True}, myquery]}
-    elif args.pol and args.loners:
-        myquery = {"$and": [{"rmsynth1d": True}, {
-            "n_components": 1}, {"polarized": True}]}
+    query = {
+        '$and':  [
+            {f'beams.{field}': {'$exists': True}},
+            {f'beams.{field}.DR1': True}
+        ]
+    }
 
-    else:
-        myquery = {"rmsynth1d": True}
+    beams = beams_col.find(query).sort('Source_ID')
+    all_island_ids = sorted(beams_col.distinct('Source_ID', query))
 
-    mydoc = mycol.find(myquery).sort("flux_peak", -1)
-    count = mycol.count_documents(myquery)
+    query = {
+        '$and': [
+            {
+                'Source_ID': {'$in': all_island_ids}
+            },
+            {
+                'rmsynth3d': True
+            }
+        ]
+    }
+
+    islands = isl_col.find(query).sort('Source_ID')
+    island_ids = [doc['Source_ID'] for doc in islands]
+    n_island = isl_col.count_documents(query)
+
+    query = {
+        '$and': [
+            {
+                'Source_ID': {'$in': all_island_ids}
+            },
+            {
+                'rmsynth1d': True
+            }
+        ]
+    }
+
+    components = comp_col.find(query).sort('Source_ID')
+    component_ids = [doc['Component_ID'] for doc in components]
+    n_comp = comp_col.count_documents(query)
 
     if args.limit is not None:
         count = args.limit
-
-    if verbose:
-        print(f'Running RM-CLEAN on {count} sources')
+        n_comp = count
+        n_island = count
+        island_ids = island_ids[:count]
+        component_ids = component_ids[:count]
 
     if args.dimension == '1d':
-        inputs = [[i, args, outdir, verbose]
-                  for i in range(count)]
+        if verbose:
+            print(f'Running RM-CLEAN on {n_comp} components')
+        inputs = [[comp_id, args, outdir, host, field, verbose]
+                  for i, comp_id in enumerate(component_ids)]
         if (pool.__class__.__name__ is 'MPIPool' or
                 pool.__class__.__name__ is 'SerialPool'):
             if verbose:
@@ -234,15 +215,17 @@ def main(pool, args, verbose=False):
         elif pool.__class__.__name__ is 'MultiPool':
             list(tqdm(
                 pool.imap_unordered(rmclean1d, inputs),
-                total=count,
+                total=n_comp,
                 desc='Running 1D RM-CLEAN',
                 disable=(not verbose)
             )
             )
 
     elif args.dimension == '3d':
-        inputs = [[i, args, outdir, verbose]
-                  for i in range(count)]
+        if verbose:
+            print(f'Running RM-CLEAN on {n_island} islands')
+        inputs = [[island_id, args, outdir, host, field, verbose]
+                  for i, island_id in enumerate(island_ids)]
         if (pool.__class__.__name__ is 'MPIPool' or
                 pool.__class__.__name__ is 'SerialPool'):
             if verbose:
@@ -256,7 +239,7 @@ def main(pool, args, verbose=False):
         elif pool.__class__.__name__ is 'MultiPool':
             list(tqdm(
                 pool.imap_unordered(rmclean3d, inputs),
-                total=count,
+                total=n_island,
                 desc='Running 3D RM-CLEAN',
                 disable=(not verbose)
             )
@@ -305,12 +288,23 @@ def cli():
     # Parse the command line options
     parser = argparse.ArgumentParser(description=descStr,
                                      formatter_class=argparse.RawTextHelpFormatter)
-
+    parser.add_argument(
+        "field",
+        metavar="field",
+        type=str,
+        help="RACS field to mosaic - e.g. 2132-50A."
+    )
     parser.add_argument(
         'outdir',
         metavar='outdir',
         type=str,
         help='Directory containing cutouts (in subdir outdir/cutouts).')
+
+    parser.add_argument(
+        'host',
+        metavar='host',
+        type=str,
+        help='Host of mongodb (probably $hostname -i).')
 
     parser.add_argument("--dimension", dest="dimension", default="1d",
                         help="How many dimensions for RMsynth [1d] or '3d'.")
@@ -325,20 +319,11 @@ def cli():
         help="Add data to MongoDB [False]."
     )
 
-    parser.add_argument("--pol", dest="pol", action="store_true",
-                        help="Run on polarized sources [False].")
-
-    parser.add_argument("--unres", dest="unres", action="store_true",
-                        help="Run on unresolved sources [False].")
-
     parser.add_argument("--validate", dest="validate", action="store_true",
                         help="Run on Stokes I [False].")
 
     parser.add_argument("--limit", dest="limit", default=None,
                         type=int, help="Limit number of sources [All].")
-
-    parser.add_argument("--loners", dest="loners", action="store_true",
-                        help="Run on single component sources [False].")
 
     # RM-tools args
     parser.add_argument("-c", dest="cutoff", type=float, default=-3,
@@ -376,10 +361,11 @@ def cli():
     if verbose:
         print(f"Using pool: {pool.__class__.__name__}")
 
-    if args.database:
-        if verbose:
-            print('Testing MongoDB connection...')
-        client = pymongo.MongoClient()  # default connection (ie, local)
+    host = args.host
+    if verbose:
+        print('Testing MongoDB connection...')
+    # default connection (ie, local)
+    with pymongo.MongoClient(host=host) as client:
         try:
             client.list_database_names()
         except pymongo.errors.ServerSelectionTimeoutError:
@@ -387,7 +373,6 @@ def cli():
         else:
             if verbose:
                 print('MongoDB connection succesful!')
-        client.close()
 
     main(pool, args, verbose=verbose)
 
