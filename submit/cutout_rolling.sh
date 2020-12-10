3f5c4c5706105a624752559ef489231fdfb2c590
Thomson, Alec (CASS, Kensington) <Alec.Thomson@csiro.au>
2020-12-10 11:04:48 +0800

    Batch scripts

--- submit/cutout_rolling.sh
+++ submit/cutout_rolling.sh
@@ -8,8 +8,8 @@
 #SBATCH --account=askap
 #SBATCH -M galaxy
 #SBATCH -p workq
-#SBATCH --nodes=50
-#SBATCH --ntasks=1000
+#SBATCH --nodes=100
+#SBATCH --ntasks=2000
 #SBATCH --time=24:00:00
 
 export OMP_NUM_THREADS=1
@@ -23,6 +23,6 @@ cd /group/askap/athomson/repos/spiceracs
 
 numactl --interleave=all mongod --dbpath=database --bind_ip $(hostname -i) &
 
-srun -n 1000 python spiceracs/cutout_rolling.py /group/askap/athomson/projects/RACS/VAST_2132-50A 2132-50A $(hostname -i) -v -vw --mpi
+srun -n 2000 python spiceracs/cutout_rolling.py /group/askap/athomson/projects/RACS/VAST_2132-50A 2132-50A $(hostname -i) -v -vw --mpi
 
 echo 'done'
