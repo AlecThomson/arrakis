9d2ba62b5916adf2e8d49e9c9be7bc05e9ef1031
Thomson, Alec (CASS, Kensington) <Alec.Thomson@csiro.au>
2021-04-29 10:17:15 +0800

    Switch to making images

--- scripts/check_cutout.py
+++ scripts/check_cutout.py
@@ -1,56 +1,62 @@
-from tqdm import tqdm
-import argparse
+from astropy.table import Table
+from spectral_cube import SpectralCube
 from glob import glob
+import os
+import matplotlib.pyplot as plt
+import astropy.units as u 
+from matplotlib.patches import Ellipse
 
-def main(total, checkfile):
-    with tqdm(total=total) as pbar:
-        while True:
-            current = len(glob(f'{checkfile}/*/*.fits'))
-            pbar.update(current - pbar.n)
-            if current == total:
-                break
-
-def cli():
-        # Help string to be shown using the -h option
-    logostr = """
-     mmm   mmm   mmm   mmm   mmm
-     )-(   )-(   )-(   )-(   )-(
-    ( S ) ( P ) ( I ) ( C ) ( E )
-    |   | |   | |   | |   | |   |
-    |___| |___| |___| |___| |___|
-     mmm     mmm     mmm     mmm
-     )-(     )-(     )-(     )-(
-    ( R )   ( A )   ( C )   ( S )
-    |   |   |   |   |   |   |   |
-    |___|   |___|   |___|   |___|
-
-    """
-
-    descStr = f"""
-    {logostr}
-
-    Monitor cutout progress
-
-    """
-    # Parse the command line options
-    parser = argparse.ArgumentParser(
-        description=descStr, formatter_class=argparse.RawTextHelpFormatter
+def plot_comp(row, c='b'):
+    ax = plt.gca()
+    ax.plot(
+        row['RA']*u.deg, 
+        row['Dec']*u.deg, 
+        marker='X', 
+        color=c,
+        transform=ax.get_transform('world')
     )
-    parser.add_argument(
-        'total',
-        metavar='total',
-        type=float,
-        help='Total number of cutouts.')
+    e = Ellipse(
+        (row['RA']*u.deg, row['Dec']*u.deg),
+        width=row['Maj'],
+        height=row['Min'],
+        angle=row['PA'],
+        transform=ax.get_transform('world')
+    )
+    ax.add_artist(e)
 
-    parser.add_argument(
-        'checkfile',
-        metavar='checkfile',
-        type=str,
-        help='Directory to monitor.')
 
-    args = parser.parse_args()
 
-    main(args.total, args.checkfile)
+def main(cutdir, source):
+    cat = Table.read(
+        '/home/athomson/athomson/projects/RACS/Catalogues/RACS-25asec-Mosaiced_Gaussians_Final_GalCut_v2020_11_15.fits'
+    )
+    cat_S = Table.read(
+        '/home/athomson/athomson/projects/RACS/Catalogues/RACS-25asec-Mosaiced_Sources_Final_GalCut_v2020_11_15.fits'
+    )
+    cat.add_index('Source_ID')
+    cat_S.add_index('Source_ID')
+    sub = Table(cat.loc[source])
+    cat_S.add_index('Source_ID')
+    sub_S = Table(cat_S.loc[source])
+
+    cutdir = os.path.abspath(cutdir)
+    images = glob(f"{cutdir}/{source}/*.image.*.i.*.linmos.fits")
+
+    cube = SpectralCube.read(images[0])
+    mom0 = cube.sum(axis=0)
+
+    fig = plt.figure()
+    ax = plt.subplot(projection=mom0.wcs)
+    ax.imshow(mom0.value)
+    ax.plot(sub['RA']*u.deg, sub['Dec']*u.deg, 'rX')
+    ax.plot(sub_S['RA']*u.deg, sub_S['Dec']*u.deg, 'bX')
+    ra = ax.coords['ra'] 
+    dec = ax.coords['dec']
+    ra.set_major_formatter('d.ddd')
+    dec.set_major_formatter('d.ddd')
+    plt.savefig('test.png')
+
+
 
 if __name__ == "__main__":
-    cli()
+    main()
