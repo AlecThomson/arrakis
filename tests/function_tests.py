ec89c83ad2b17baf3fa2586f3ec8e2a001ff9875
Thomson, Alec (S&A, Kensington) <Alec.Thomson@csiro.au>
2021-10-29 10:10:07 +0800

    Add tests

---
+++ tests/function_tests.py
@@ -0,0 +1,10 @@
+"""Tests for functions."""
+
+import unittest
+
+class UnitTests(unittest.TestCase):
+    def test_somthing(self):
+        self.assertEqual(0,0)
+
+if __name__ == '__main__':
+    unittest.main()
