a15972674801dc4e0a088f9f79a172d7c3f3b991
Thomson, Alec (S&A, Kensington) <Alec.Thomson@csiro.au>
2022-03-25 17:06:37 +0800

    Fix for RMTable compatibility

--- spiceracs/makecat.py
+++ spiceracs/makecat.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """Make a SPICE-RACS catalogue"""
+import os
 import numpy as np
 import warnings
 from astropy.table import QTable, Column
 from astropy.io import fits
-import pymongo
+import astropy.units as u
 from tqdm import tqdm, trange
 from spiceracs import columns_possum
-from spiceracs.utils import get_db, test_db
+from spiceracs.utils import get_db, test_db, get_field_db
 import rmtable as RMT
 import logging as log
 from pprint import pformat
@@ -28,16 +29,15 @@ def lognorm_from_percentiles(x1, p1, x2, p2):
     scale = (x2 - x1) / (p2ppf - p1ppf)
     mean = ((x1 * p2ppf) - (x2 * p1ppf)) / (p2ppf - p1ppf)
 
-#     return lognorm(s=scale, scale=np.exp(mean))
     return scale, np.exp(mean)
 
 
 def sigma_add_fix(tab):
-    sigma_Q_low = np.array(tab['Sigma_add_Q'] - tab['E_Sigma_add_minus_Q'])
-    sigma_Q_high = np.array(tab['Sigma_add_Q'] + tab['E_Sigma_add_plus_Q'])
+    sigma_Q_low = np.array(tab['sigma_add_Q'] - tab['sigma_add_Q_err_minus'])
+    sigma_Q_high = np.array(tab['sigma_add_Q'] + tab['sigma_add_Q_err_plus'])
 
-    sigma_U_low = np.array(tab['Sigma_add_U'] - tab['E_Sigma_add_minus_U'])
-    sigma_U_high = np.array(tab['Sigma_add_U'] + tab['E_Sigma_add_plus_U'])
+    sigma_U_low = np.array(tab['sigma_add_U'] - tab['sigma_add_U_err_minus'])
+    sigma_U_high = np.array(tab['sigma_add_U'] + tab['sigma_add_U_err_plus'])
 
     s_Q, scale_Q = lognorm_from_percentiles(
         sigma_Q_low,
@@ -56,7 +56,8 @@ def sigma_add_fix(tab):
     med, std = np.zeros_like(s_Q), np.zeros_like(s_Q)
     for i, (_s_Q, _scale_Q, _s_U, _scale_U) in tqdm(
         enumerate(zip(s_Q, scale_Q, s_U, scale_U)),
-        total=len(s_Q)
+        total=len(s_Q),
+        desc="Calculating sigma_add"
     ):
         try:
             Q_dist = lognorm.rvs(s=_s_Q, scale=_scale_Q, size=(1000))
@@ -68,16 +69,16 @@ def sigma_add_fix(tab):
             med[i] = np.nan
             std[i] = np.nan
 
-    tab.add_column(Column(data=med, name='Sigma_add_P'))
-    tab.add_column(Column(data=std, name='E_Sigma_add_P'))
+    tab.add_column(Column(data=med, name='sigma_add'))
+    tab.add_column(Column(data=std, name='sigma_add_err'))
     tab.remove_columns(
         [
-            'Sigma_add_Q',
-            'Sigma_add_U',
-            'E_Sigma_add_minus_Q',
-            'E_Sigma_add_plus_Q',
-            'E_Sigma_add_minus_U',
-            'E_Sigma_add_plus_U'
+            'sigma_add_Q',
+            'sigma_add_U',
+            'sigma_add_Q_err_minus',
+            'sigma_add_Q_err_plus',
+            'sigma_add_U_err_minus',
+            'sigma_add_U_err_plus'
         ]
     )
 
@@ -110,14 +111,13 @@ def get_fit_func(tab, nbins=21, offset=0.002):
         np.polynomial.Polynomial.fit: 3rd order polynomial fit.
     """
     # Select high SNR sources
-    hi_snr = (tab['Peak_I_flux_Gaussian'] / tab['Noise_I']) > 100
+    hi_snr = (tab['stokesI'] / tab['stokesI_err']) > 100
     hi_i_tab = tab[hi_snr]
     # Get fractional pol
-    frac_P = np.array(hi_i_tab['Fractional_polarisation']) * \
-        hi_i_tab['Fractional_polarisation'].unit
+    frac_P = np.array(hi_i_tab['fracpol'].value)
     # Bin sources by separation from tile centre
     bins = np.histogram_bin_edges(
-        hi_i_tab['Separation_Tile_Centre'], bins=nbins)
+        hi_i_tab['beamdist'].to(u.deg).value, bins=nbins)
     bins_c = np.median(np.vstack([bins[0:-1], bins[1:]]), axis=0)
     # Compute the median and standard deviation of the fractional pol
     meds = np.zeros_like(bins_c)
@@ -126,13 +126,18 @@ def get_fit_func(tab, nbins=21, offset=0.002):
     s2_ups = np.zeros_like(bins_c)
     s2_los = np.zeros_like(bins_c)
     for i in range(len(bins)-1):
-        idx = ((hi_i_tab['Separation_Tile_Centre'] < bins[i+1])
-               & (hi_i_tab['Separation_Tile_Centre'] >= bins[i]))
+        idx = ((hi_i_tab['beamdist'].to(u.deg).value < bins[i+1])
+               & (hi_i_tab['beamdist'].to(u.deg).value >= bins[i]))
         s2_los[i], s1_los[i], meds[i], s1_ups[i], s2_ups[i] = np.nanpercentile(
             frac_P[idx], [2.3, 16, 50, 84, 97.6]
         )
     # Fit to median with small offset
-    fit = np.polynomial.Polynomial.fit(bins_c, meds+offset, deg=3, full=False)
+    fit = np.polynomial.Polynomial.fit(
+        bins_c, 
+        meds+offset, 
+        deg=3, 
+        full=False
+    )
     return fit
 
 
@@ -145,34 +150,67 @@ def cuts_and_flags(cat):
         cat (RMT): Catalogue to cut and flag
     """
     # SNR cut
-    snr_cut = cat['SNR_Polarised_intensity'] > 5
+    snr_cut = cat['snr_polint'] > 5
     cat = cat[snr_cut]
     # SNR flag
-    snr_flag = cat['SNR_Polarised_intensity'] < 8
-    cat.add_column(Column(data=snr_flag, name='SNR_flag'))
+    snr_flag = cat['snr_polint'] < 8
+    cat.add_column(Column(data=snr_flag, name='snr_flag'))
     # Leakage flag
     fit = get_fit_func(cat)
     leakage_flag = is_leakage(
-        cat['Fractional_polarisation'],
-        cat['Separation_Tile_Centre'],
+        cat['fracpol'].value,
+        cat['beamdist'].to(u.deg).value,
         fit
     )
-    cat.add_column(Column(data=leakage_flag, name='Leakage_flag'))
+    cat.add_column(Column(data=leakage_flag, name='leakage_flag'))
     # Channel flag
-    chan_flag = cat['Number_of_channels'] < 144
-    cat.add_column(Column(data=chan_flag, name='Channel_flag'))
+    chan_flag = cat['Nchan'] < 144
+    cat.add_column(Column(data=chan_flag, name='channel_flag'))
     # Fitting flag
-    fit_flag = cat['Stokes_I_fit_flag'] >= 64
-    cat.remove_column('Stokes_I_fit_flag')
-    cat.add_column(Column(data=fit_flag, name='Stokes_I_fit_flag'))
-    # Sigma_add flag
-    sigma_flag = cat['Sigma_add_P'] > 1
-    cat.add_column(Column(data=sigma_flag, name='Complex_sigma_add_flag'))
+    fit_flag = cat['stokes_I_fit_flag'] >= 64
+    cat.remove_column('stokes_I_fit_flag')
+    cat.add_column(Column(data=fit_flag, name='stokes_I_fit_flag'))
+    # sigma_add flag
+    sigma_flag = cat['sigma_add'] > 1
+    cat.add_column(Column(data=sigma_flag, name='complex_sigma_add_flag'))
     # M2_CC flag
-    m2_flag = cat['M2_CC'] > cat['RMSF_FWHM']
-    cat.add_column(Column(data=m2_flag, name='Complex_M2_CC_flag'))
+    m2_flag = cat['rm_width'] > cat['rmsf_fwhm']
+    cat.add_column(Column(data=m2_flag, name='complex_M2_CC_flag'))
 
-    return cat
+    return cat, fit
+
+def get_alpha(cat):
+    coefs_str = cat["stokesI_model_coef"]
+    alphas = []
+    for c in coefs_str:
+        coefs = c.split(",")
+        alpha = float(coefs[-1])
+        alphas.append(alpha)
+    return np.array(alphas)
+
+def get_integration_time(cat, field_col):
+    field_names = list(cat['tile_id'])
+    query = {
+        "$and": [
+            {"FIELD_NAME": {"$in": field_names}},
+            {"SELECT": 1}
+        ]
+    }
+    tint_dicts = list(field_col.find(query, {"_id":0,"SCAN_TINT": 1, "FIELD_NAME": 1}))
+    tint_dict = {}
+    for d in tint_dicts:
+        tint_dict.update(
+                {
+                    d["FIELD_NAME"]: d["SCAN_TINT"]
+                }
+        )
+
+    tints = []
+    for name in field_names:
+        tints.append(tint_dict[name])
+    
+    return np.array(tints) * u.s
+
 
 
 def main(
@@ -182,7 +220,6 @@ def main(
     password: str = None,
     verbose=True,
     outfile: str = None,
-    cat_format: str = None,
 ) -> None:
     """Main
 
@@ -197,6 +234,9 @@ def main(
     """
     # default connection (ie, local)
     beams_col, island_col, comp_col = get_db(
+        host=host, username=username, password=password
+    )
+    field_col = get_field_db(
         host=host, username=username, password=password
     )
     query = {
@@ -269,34 +309,73 @@ def main(
             data += [comp[selcol]]
         new_col = Column(data=data, name=selcol)
         tab.add_column(new_col)
+    
+    # Fix sigma_add
+    tab = sigma_add_fix(tab)
+
+    # Add flags
+    tab, fit = cuts_and_flags(tab)
+
+    # Add spectral index from fitted model
+    alphas = get_alpha(tab)
+    tab.add_column(Column(data=alphas, name='spectral_index'))
+
+    # Add integration time
+    tints = get_integration_time(tab, field_col)
+    tab.add_column(Column(data=tints, name='int_time'))
+    # Add epoch
+    tab.add_column(Column(data=tab['start_time'] + (tints / 2), name='epoch'))
+
+    # Convert to rmtab
     rmtab = RMT.from_table(tab)
     # Get Galatic coords
-    rmtab["Gal_lon"], rmtab["Gal_lat"] = RMT.calculate_missing_coordinates_column(
-        rmtab["RA"], rmtab["Dec"], to_galactic=True
+    glon, glat = RMT.calculate_missing_coordinates_column(
+        rmtab["ra"], rmtab["dec"], to_galactic=True
     )
+    rmtab.add_column(data=glon, name="l")
+    rmtab.add_column(data=glat, name="b")
+    rmtab.add_column(data=np.max([rmtab['ra_err'], rmtab['dec_err']]), name="pos_err")
 
     # Add common columns
-    rmtab["rm_method"] = "RM Synthesis"
+    rmtab["rm_method"] = "RM Synthesis - Fractional polarization"
     rmtab["telescope"] = "ASKAP"
     rmtab["pol_bias"] = "2012PASA...29..214G"
+    rmtab["catalog"] = "SPICE-RACS-DR1"
+    rmtab["ionosphere"] = "FRion"
+    rmtab["flux_type"] = "Peak"
+    rmtab["aperture"] = 0*u.deg
+
+    rmtab.add_column(
+        data=fit(
+            rmtab["distance_from_beam_center"].to(u.deg).value,
+        ),
+        name="leakage"
+    )
+
+    rmtab.add_column(
+        data=np.logical_or(
+            rmtab['complex_sigma_add_flag'], 
+            rmtab['complex_M2_CC_flag']
+        ), 
+        name='complex_flag'
+    )
 
     # Verify table
     rmtab.verify_columns()
-    # rmtab.verify_standard_strings()
-    rmtab.verify_limits()
     rmtab.add_missing_columns()
+    # Readd complex test
+    rmtab["complex_test"]  = "sigma_add OR Second moment"
 
-    # Fix sigma_add
-    rmtab = sigma_add_fix(rmtab)
-
-    # Add flags
-    rmtab = cuts_and_flags(rmtab)
 
     if outfile is None:
         log.info(pformat(rmtab))
 
     if outfile is not None:
-        rmtab.table.write(outfile, format=cat_format, overwrite=True)
+        fmt = None
+        _ , ext = os.path.splitext(outfile)
+        if ext == ".xml" or ext == ".vot":
+            fmt = "votable"
+        rmtab.table.write(outfile, overwrite=True, format=fmt)
         log.info(f"{outfile} written to disk")
 
     log.info("Done!")
@@ -370,18 +449,7 @@ def cli():
         help="File to save table to [None].",
     )
 
-    parser.add_argument(
-        "-f",
-        "--format",
-        dest="format",
-        default=None,
-        type=str,
-        help="Format for output file [None].",
-    )
-
     args = parser.parse_args()
-    if args.outfile and not args.format:
-        parser.error("Please provide an output file format.")
 
     verbose = args.verbose
 
@@ -411,7 +479,6 @@ def cli():
         password=args.password,
         verbose=verbose,
         outfile=args.outfile,
-        cat_format=args.format,
     )
 
 
