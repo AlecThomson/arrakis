e9bd27978e96e22fa5780ae022c44c206715b797
Thomson, Alec (S&A, Kensington) <Alec.Thomson@csiro.au>
2022-06-10 10:52:31 +0800

    Fix for zeus

--- submit/casda_prepare.sh
+++ submit/casda_prepare.sh
@@ -26,6 +26,7 @@
 #SBATCH --partition=highmemq
 
 # conda activate spice
+module load intel-mpi
 conda activate spice-zeus
 
 data_dir=/group/ja3/athomson/full_spica
@@ -34,6 +35,6 @@ polcat=/group/ja3/athomson/spice-racs.dr1.corrected.xml
 cd $data_dir
 # srun -n $SLURM_NTASKS casda_prepare.py $data_dir $polcat --update-cubes --convert-spectra --convert-plots -v --mpi --batch_size 10_000
 # srun -n $SLURM_NTASKS casda_prepare.py $data_dir $polcat --convert-plots -v --mpi --batch_size 10_000
-srun -n $SLURM_NTASKS casda_prepare.py $data_dir $polcat --convert-spectra -v --mpi --batch_size 10_000 --interface ib0
+srun -n $SLURM_NTASKS casda_prepare.py $data_dir $polcat --convert-spectra -v --mpi --batch_size 10_000 --interface ib0 --outdir /scratch/ja3/athomson/spica
 # srun -n 20 casda_prepare.py $data_dir $polcat --convert-spectra --convert-plots -v --mpi --batch_size 1000
 # srun -n 20 casda_prepare.py $data_dir $polcat --convert-spectra -v --mpi --batch_size 10_000
