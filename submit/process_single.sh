a03d7bd2a336c8ea7f555936e22f22d687d797e9
Thomson, Alec (S&A, Kensington) <Alec.Thomson@csiro.au>
2022-06-23 09:41:57 +0800

    Add single field script

---
+++ submit/process_single.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -l
+#SBATCH --job-name=processSPICE
+#SBATCH --export=NONE
+#SBATCH --mail-user=alec.thomson@csiro.au
+#SBATCH --mail-type=ALL
+#SBATCH -e /group/askap/athomson/projects/spiceracs/spica/slurmLogs/slurm-%j.err
+#SBATCH -o /group/askap/athomson/projects/spiceracs/spica/slurmLogs/slurm-%j.out
+#SBATCH --ntasks=500
+#SBATCH --requeue
+#SBATCH --ntasks-per-node=20
+#SBATCH --time=12:00:00
+#SBATCH --cluster=galaxy
+#SBATCH --account=askaprt
+
+export OMP_NUM_THREADS=1
+conda activate spice
+module load singularity
+export SINGULARITY_BINDPATH=$(pwd),/group
+
+field='1416+00A'
+
+echo Running pipeline on $field
+cal_sbid=`find_sbid.py $field --cal`
+weight=`find_sbid.py $field --weight`
+weight_pad=`printf "%05d\n" $weight`
+zernike=/group/askap/athomson/projects/spiceracs/leakages_bak/${weight_pad}_zernike_holo_cube.fits
+data_dir=/group/ja3/athomson/spica
+config=/group/askap/athomson/projects/spiceracs/spica/spica_config.txt
+
+# Image dirctory
+cd $data_dir
+
+# Make a copy of this sbatch file for posterity
+sedstr="s/sbatch/${field}.${SLURM_JOB_ID}\.sbatch/g"
+slurmdir="/group/askap/athomson/projects/spiceracs/spica/slurmFiles"
+currentdir="/group/askap/athomson/repos/spiceracs/submit"
+sedstr2="s+${currentdir}+${slurmdir}+g"
+
+# Correct for leakage with Zernike file
+echo "Correcting for leakage"
+srun -n 500 --export=ALL spice_process $field $data_dir/$cal_sbid/RACS_test4_1.05_$field --config $config --savePlots --use_mpi --skip_cutout --holofile $zernike
