824afa75b096a3257feda2676b03e93383e0ddd6
Thomson, Alec (S&A, Kensington) <Alec.Thomson@csiro.au>
2022-08-26 15:58:09 +0800

    Test run

--- submit/casda_prepare.sh
+++ submit/casda_prepare.sh
@@ -11,7 +11,7 @@
 #SBATCH --account=askap
 #SBATCH --ntasks=48
 #SBATCH --time=0-12:00:00
-#SBATCH --partition=highmemq
+##SBATCH --partition=highmemq
 
 # conda activate spice
 module load intel-mpi
@@ -22,4 +22,4 @@ polcat=/group/ja3/athomson/spice-racs.dr1.corrected.xml
 
 cd $data_dir
 
-srun -n $SLURM_NTASKS casda_prepare.py $data_dir $polcat --update-cubes --convert-spectra --convert-plots -v --mpi --batch_size 10_000 --interface ib0 --outdir /scratch/ja3/athomson/spica
+srun -n $SLURM_NTASKS casda_prepare.py $data_dir $polcat --update-cubes --convert-spectra --convert-plots -v --mpi --batch_size 10_000 --interface ib0 --outdir /scratch/ja3/athomson/spica --test
