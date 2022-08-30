bac2801ceb9eed29140dbc5fde19d2ffbe4e9d9e
Thomson, Alec (S&A, Kensington) <Alec.Thomson@csiro.au>
2022-08-30 15:12:39 +0800

    Run in two goes

--- submit/casda_prepare.sh
+++ submit/casda_prepare.sh
@@ -10,7 +10,7 @@
 #SBATCH --cluster=zeus
 #SBATCH --account=askap
 #SBATCH --ntasks=48
-#SBATCH --time=0-12:00:00
+#SBATCH --time=1-00:00:00
 ##SBATCH --partition=highmemq
 
 # conda activate spice
@@ -22,4 +22,5 @@ polcat=/group/ja3/athomson/spice-racs.dr1.corrected.xml
 
 cd $data_dir
 
-srun -n $SLURM_NTASKS casda_prepare.py $data_dir $polcat --update-cubes --convert-spectra --convert-plots -v --mpi --batch_size 10_000 --interface ib0 --outdir /scratch/ja3/athomson/spica --test
+srun -n $SLURM_NTASKS casda_prepare.py $data_dir $polcat --convert-spectra -v --mpi --batch_size 10_000 --interface ib0 --outdir /scratch/ja3/athomson/spica
+srun -n $SLURM_NTASKS casda_prepare.py $data_dir $polcat --update-cubes --convert-plots -v --mpi --batch_size 10_000 --interface ib0 --outdir /scratch/ja3/athomson/spica
