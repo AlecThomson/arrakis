cd3dc9a45feceaf6c5115ca80993d5d3f88b1270
Thomson, Alec (S&A, Kensington) <Alec.Thomson@csiro.au>
2022-10-19 10:00:05 +0800

    Clean up tests

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -6,13 +6,13 @@ import unittest
 
 class test_cli(unittest.TestCase):
     def test_cli_init(self):
-        """Tests that the CLI `initSPICE` runs."""
-        res = subprocess.run(['initSPICE', '--help'], check=True)
+        """Tests that the CLI `spice_init` runs."""
+        res = subprocess.run(['spice_init', '--help'], check=True)
         self.assertEqual(res.returncode, 0)
 
-    def test_cli_processSPICE(self):
-        """Tests that the CLI `processSPICE` runs."""
-        res = subprocess.run(['processSPICE', '--help'], check=True)
+    def test_cli_process(self):
+        """Tests that the CLI `spice_process` runs."""
+        res = subprocess.run(['spice_process', '--help'], check=True)
         self.assertEqual(res.returncode, 0)
 
 
