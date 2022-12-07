b9d88ccf0f26b325e75dca9a8201eb64fb08845a
Thomson, Alec (S&A, Kensington) <Alec.Thomson@csiro.au>
2022-12-07 14:41:29 +0800

    Processing

--- submit/casda_prepare.sh
+++ submit/casda_prepare.sh
@@ -6,17 +6,18 @@
 #SBATCH --mail-type=ALL
 #SBATCH -e /group/askap/athomson/projects/spiceracs/spica/slurmLogs/casda_prep_slurm-%j.log
 #SBATCH -o /group/askap/athomson/projects/spiceracs/spica/slurmLogs/casda_prep_slurm-%j.log
-
 #SBATCH --cluster=galaxy
 #SBATCH --account=askap
-#SBATCH --ntasks=100
-#SBATCH --time=2-00:00:00
+#SBATCH --ntasks=1000
+#SBATCH --ntasks-per-node=10
+#SBATCH --time=1-00:00:00
 
 # conda activate spice
 conda activate spice
 
 data_dir=/group/askap/athomson/projects/spiceracs/DR1/full_spica
-polcat=/group/askap/athomson/projects/spiceracs/DR1/spice-racs.dr1.corrected.xml
+polcat=/group/askap/athomson/projects/spiceracs/DR1/spice-racs.dr1.multiflag.corrected.xml
+prep_type=cut
 
 cd $data_dir
-srun -n $SLURM_NTASKS casda_prepare.py $data_dir $polcat --convert-spectra --convert-cubes --convert-plots -v --mpi --batch_size 10_000
+srun -n $SLURM_NTASKS casda_prepare.py $data_dir $polcat $prep_type --convert-spectra --convert-cubes --convert-plots -v --mpi --batch_size 10_000
