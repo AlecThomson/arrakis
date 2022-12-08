c0a266f899eda32306a7b246363323ea7a25ea60
Thomson, Alec (S&A, Kensington) <Alec.Thomson@csiro.au>
2022-12-08 09:36:27 +0800

    Full cut

--- submit/casda_prepare.sh
+++ submit/casda_prepare.sh
@@ -17,7 +17,7 @@ conda activate spice
 
 data_dir=/group/askap/athomson/projects/spiceracs/DR1/full_spica
 polcat=/group/askap/athomson/projects/spiceracs/DR1/spice-racs.dr1.multiflag.corrected.xml
-prep_type=cut
+prep_type=full
 
 cd $data_dir
 srun -n $SLURM_NTASKS casda_prepare.py $data_dir $polcat $prep_type --convert-spectra --convert-cubes --convert-plots -v --mpi --batch_size 10_000
