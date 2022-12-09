a921e465e7c00e95ac7e36da2b01078313231a83
Thomson, Alec (S&A, Kensington) <Alec.Thomson@csiro.au>
2022-12-09 17:06:35 +0800

    Running

--- submit/casda_prepare.sh
+++ submit/casda_prepare.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -l
-
 #SBATCH --job-name=SPICE-CASDA
 #SBATCH --export=NONE
 #SBATCH --mail-user=alec.thomson@csiro.au
@@ -8,7 +7,7 @@
 #SBATCH -o /group/askap/athomson/projects/spiceracs/spica/slurmLogs/casda_prep_slurm-%j.log
 #SBATCH --cluster=galaxy
 #SBATCH --account=askap
-#SBATCH --ntasks=10
+#SBATCH --ntasks=1000
 #SBATCH --ntasks-per-node=10
 #SBATCH --time=1-00:00:00
 
@@ -16,9 +15,8 @@
 conda activate spice
 data_dir=/group/askap/athomson/projects/spiceracs/DR1/full_spica
 polcat=/group/askap/athomson/projects/spiceracs/DR1/spice-racs.dr1.corrected.xml
-prep_type=test
+prep_type=cut
 
 cd $data_dir
-# srun -n $SLURM_NTASKS casda_prepare.py $data_dir $polcat $prep_type --convert-spectra --convert-cubes --convert-plots -v --mpi --batch_size 10_000
-srun -n $SLURM_NTASKS /group/askap/athomson/repos/spiceracs/scripts/casda_prepare.py $data_dir $polcat $prep_type --convert-spectra --convert-cubes --convert-plots -v --mpi --batch_size 10_000
+srun -n $SLURM_NTASKS casda_prepare.py $data_dir $polcat $prep_type --convert-spectra --convert-cubes --convert-plots -v --mpi --batch_size 10_000
 
