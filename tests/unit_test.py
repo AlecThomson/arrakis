e0e8e9751ff98e0b71a9549c9740a05932e95879
Thomson, Alec (S&A, Kensington) <Alec.Thomson@csiro.au>
2023-02-06 13:03:29 +0800

    Mv unit

---
+++ tests/unit_test.py
@@ -0,0 +1,40 @@
+"""Tests for functions."""
+
+import unittest
+
+from spiceracs.rmsynth_oncuts import (
+    rmsynthoncut3d,
+    rms_1d,
+    estimate_noise_annulus,
+    rmsynthoncut1d,
+    rmsynthoncut_i,
+)
+
+# Test functions within spiceracs.rmsyth_oncuts
+
+class TestRmsynthOncuts(unittest.TestCase):
+    """Test rmsynth_oncuts functions."""
+
+    def test_rmsynthoncut3d(self):
+        """Test rmsynthoncut3d."""
+        pass
+
+    def test_rms_1d(self):
+        """Test rms_1d."""
+        pass
+
+    def test_estimate_noise_annulus(self):
+        """Test estimate_noise_annulus."""
+        pass
+
+    def test_rmsynthoncut1d(self):
+        """Test rmsynthoncut1d."""
+        pass
+
+    def test_rmsynthoncut_i(self):
+        """Test rmsynthoncut_i."""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
