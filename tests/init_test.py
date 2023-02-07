ca5b361b62094a7c5a900e415282b5088c3f3675
Thomson, Alec (S&A, Kensington) <Alec.Thomson@csiro.au>
2023-02-07 17:37:51 +0800

    Dont run this

--- tests/init_test.py
+++ tests/init_test.py
@@ -1,96 +1,98 @@
-"""Tests for SPICE init."""
+# """Tests for SPICE init."""
 
-import unittest
-import subprocess as sp
-import socket
-import time
-from typing import Tuple, Union
-import os
-import shutil
+# import unittest
+# import subprocess as sp
+# import socket
+# import time
+# from typing import Tuple, Union
+# import os
+# import shutil
 
-import pymongo
-from spiceracs.logger import logger
+# import pymongo
+# from spiceracs.logger import logger
 
-logger.setLevel("DEBUG")
+# logger.setLevel("DEBUG")
 
-from spiceracs.init_database import (
-    main,
-    source2beams,
-    ndix_unique,
-    cat2beams,
-    source_database,
-    beam_database,
-    get_catalogue,
-    get_beams,
-    field_database,
-)
+# from spiceracs.init_database import (
+#     main,
+#     source2beams,
+#     ndix_unique,
+#     cat2beams,
+#     source_database,
+#     beam_database,
+#     get_catalogue,
+#     get_beams,
+#     field_database,
+# )
 
-SOURCE_CAT = "data/source_RACS_1237+12A_3165.fits"
-GAUSS = "data/gaussians_RACS_1237+12A_3165.fits"
-EPOCH = "data/spiceracs_test"
-FIELD = "data/RACS_1237+12A"
+# SOURCE_CAT = "data/source_RACS_1237+12A_3165.fits"
+# GAUSS = "data/gaussians_RACS_1237+12A_3165.fits"
+# EPOCH = "data/spiceracs_test"
+# FIELD = "data/RACS_1237+12A"
 
-def start_mongodb(port: int = 27017) -> Tuple[str, int]:
-    """Start a local MongoDB instance."""
-    if os.path.exists("data/testdb"):
-        shutil.rmtree("data/testdb")
+# def start_mongodb(port: int = 27017) -> Tuple[str, int]:
+#     """Start a local MongoDB instance."""
+#     return
+#     if os.path.exists("data/testdb"):
+#         shutil.rmtree("data/testdb")
 
-    os.makedirs("data/testdb")
+#     os.makedirs("data/testdb")
 
-    cmd = f"mongod --dbpath data/testdb --port {port} --fork --auth --logpath data/testdb/mongodb.log"
-    logger.debug("Starting mongo...")
-    sp.run(cmd.split(), check=True)
-    logger.debug("Mongo started. Sleeping for 5 seconds...")
-    time.sleep(5)
-    logger.debug("Mongo should be ready.")
-    host = socket.gethostbyname(socket.gethostname())
-    return host, port
+#     cmd = f"mongod --dbpath data/testdb --port {port} --fork --auth --logpath data/testdb/mongodb.log"
+#     logger.debug("Starting mongo...")
+#     sp.run(cmd.split(), check=True)
+#     logger.debug("Mongo started. Sleeping for 5 seconds...")
+#     time.sleep(5)
+#     logger.debug("Mongo should be ready.")
+#     host = socket.gethostbyname(socket.gethostname())
+#     return host, port
 
-def create_mongo_admin(
-    host: str,
-    port: int=27017,
-    username: str="admin",
-    password: str="admin",
-    ) -> None:
+# def create_mongo_admin(
+#     host: str,
+#     port: int=27017,
+#     username: str="admin",
+#     password: str="admin",
+#     ) -> None:
 
-    """Create an admin user in a local MongoDB instance."""
-
-    client = pymongo.MongoClient(host, port)
-    db = client.admin
-    cmd = db.command("createUser", username, pwd=password, roles=["root"])
-    logger.debug(cmd)
-
-
-def stop_mongodb() -> None:
-    """Stop a local MongoDB instance."""
-    cmd = f"mongod --dbpath data/testdb --shutdown"
-    logger.debug("Stopping mongo...")
-    p = sp.Popen(cmd.split(),)
-    logger.debug("Mongo stopped.")
-
-class TestInit(unittest.TestCase):
-    host, port = start_mongodb()
-    create_mongo_admin(host, port)
+#     """Create an admin user in a local MongoDB instance."""
+#     return
+#     client = pymongo.MongoClient(host, port)
+#     db = client.admin
+#     cmd = db.command("createUser", username, pwd=password, roles=["root"])
+#     logger.debug(cmd)
 
 
-    def test_main(self):
-        main(
-            load=True,
-            islandcat=SOURCE_CAT,
-            compcat=GAUSS,
-            host=self.host,
-            username="admin",
-            password="admin",
-            field=FIELD,
-            epoch=EPOCH,
-            force=True,
-        )
-        assert True
+# def stop_mongodb() -> None:
+#     """Stop a local MongoDB instance."""
+#     return
+#     cmd = f"mongod --dbpath data/testdb --shutdown"
+#     logger.debug("Stopping mongo...")
+#     p = sp.Popen(cmd.split(),)
+#     logger.debug("Mongo stopped.")
+
+# class TestInit(unittest.TestCase):
+#     host, port = start_mongodb()
+#     create_mongo_admin(host, port)
 
 
-    logger.debug("BOOP")
+#     def test_main(self):
+#         main(
+#             load=True,
+#             islandcat=SOURCE_CAT,
+#             compcat=GAUSS,
+#             host=self.host,
+#             username="admin",
+#             password="admin",
+#             field=FIELD,
+#             epoch=EPOCH,
+#             force=True,
+#         )
+#         assert True
 
-    stop_mongodb()
 
-if __name__ == '__main__':
-    unittest.main()
+#     logger.debug("BOOP")
+
+#     stop_mongodb()
+
+# if __name__ == '__main__':
+#     unittest.main()
