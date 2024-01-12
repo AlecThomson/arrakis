35b1eccf6d7cf73cf18ffcfca9bfdf4797c57567
Thomson, Alec (CASS, Kensington) <alec.thomson@csiro.au>
2024-01-12 18:01:55 +1100

    Do in mem

--- arrakis/cutout.py
+++ arrakis/cutout.py
@@ -9,7 +9,7 @@ from pprint import pformat
 from shutil import copyfile
 from typing import Dict, List
 from typing import NamedTuple as Struct
-from typing import Optional, TypeVar, Union
+from typing import Optional, Set, TypeVar, Union
 
 import astropy.units as u
 import numpy as np
@@ -47,9 +47,6 @@ T = TypeVar("T")
 class CutoutArgs(Struct):
     """Arguments for cutout function"""
 
-    image: str
-    """Name of the image file"""
-    source_id: str
     """Name of the source"""
     ra_high: float
     """Upper RA bound in degrees"""
@@ -61,35 +58,54 @@ class CutoutArgs(Struct):
     """Lower DEC bound in degrees"""
     outdir: str
     """Output directory"""
-    beam: int
-    """Beam number"""
-    stoke: str
-    """Stokes parameter"""
 
 
-@task(name="Cutout island")
-def cutout(
+def cutout_weight(
+    image_name: str,
+    source_id: str,
     cutout_args: CutoutArgs,
     field: str,
+    dryrun=False,
+) -> pymongo.UpdateOne:
+    image = image_name.replace("image.restored", "weights.restored").replace(
+        ".fits", ".txt"
+    )
+    outfile = outfile.replace("image.restored", "weights.restored").replace(
+        ".fits", ".txt"
+    )
+
+    if not dryrun:
+        copyfile(image, outfile)
+        logger.info(f"Written to {outfile}")
+
+    # Update database
+    myquery = {"Source_ID": source_id}
+
+    filename = os.path.join(
+        os.path.basename(os.path.dirname(outfile)), os.path.basename(outfile)
+    )
+    newvalues = {
+        "$set": {
+            f"beams.{field}.{cutout_args.stoke}_beam{cutout_args.beam}_weight_file": filename
+        }
+    }
+
+    return pymongo.UpdateOne(myquery, newvalues, upsert=True)
+
+
+def cutout_image(
+    image_name: str,
+    data_in_mem: np.ndarray,
+    cube: SpectralCube,
+    source_id: str,
+    cutout_args: CutoutArgs,
+    field: str,
+    beam_num: int,
+    stoke: str,
     pad=3,
     dryrun=False,
-) -> List[pymongo.UpdateOne]:
+) -> pymongo.UpdateOne:
     """Perform a cutout.
-
-    Args:
-        image (str): Name of the image file
-        src_name (str): Name of the RACS source
-        beam (int): Beam number
-        ra_high (float): Upper RA bound
-        ra_low (float): Lower RA bound
-        dec_high (float): Upper DEC bound
-        dec_low (float): Lower DEC bound
-        outdir (str): Output directgory
-        stoke (str): Stokes parameter
-        field (str): RACS field name
-        pad (int, optional): Number of beamwidths to pad. Defaults to 3.
-        verbose (bool, optional): Verbose output. Defaults to False.
-        dryrun (bool, optional): Don't save FITS files. Defaults to False.
 
     Returns:
         pymongo.UpdateOne: Update query for MongoDB
@@ -99,111 +115,68 @@ def cutout(
     outdir = os.path.abspath(cutout_args.outdir)
 
     ret = []
-    for imtype in ["image", "weight"]:
-        basename = os.path.basename(cutout_args.image)
-        outname = f"{cutout_args.source_id}.cutout.{basename}"
-        outfile = os.path.join(outdir, outname)
+    basename = os.path.basename(image_name)
+    outname = f"{source_id}.cutout.{basename}"
+    outfile = os.path.join(outdir, outname)
 
-        if imtype == "weight":
-            image = cutout_args.image.replace(
-                "image.restored", "weights.restored"
-            ).replace(".fits", ".txt")
-            outfile = outfile.replace("image.restored", "weights.restored").replace(
-                ".fits", ".txt"
-            )
-            copyfile(image, outfile)
-            logger.info(f"Written to {outfile}")
+    padder = cube.header["BMAJ"] * u.deg * pad
 
-        if imtype == "image":
-            logger.info(f"Reading {cutout_args.image}")
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", AstropyWarning)
-                cube = SpectralCube.read(
-                    cutout_args.image, memmap=True, mode="denywrite"
-                )
-            padder = cube.header["BMAJ"] * u.deg * pad
+    xlo = Longitude(cutout_args.ra_low * u.deg) - Longitude(padder)
+    xhi = Longitude(cutout_args.ra_high * u.deg) + Longitude(padder)
+    ylo = Latitude(cutout_args.dec_low * u.deg) - Latitude(padder)
+    yhi = Latitude(cutout_args.dec_high * u.deg) + Latitude(padder)
 
-            xlo = Longitude(cutout_args.ra_low * u.deg) - Longitude(padder)
-            xhi = Longitude(cutout_args.ra_high * u.deg) + Longitude(padder)
-            ylo = Latitude(cutout_args.dec_low * u.deg) - Latitude(padder)
-            yhi = Latitude(cutout_args.dec_high * u.deg) + Latitude(padder)
+    xp_lo, yp_lo = skycoord_to_pixel(SkyCoord(xlo, ylo), cube.wcs)
+    xp_hi, yp_hi = skycoord_to_pixel(SkyCoord(xhi, yhi), cube.wcs)
 
-            xp_lo, yp_lo = skycoord_to_pixel(SkyCoord(xlo, ylo), cube.wcs)
-            xp_hi, yp_hi = skycoord_to_pixel(SkyCoord(xhi, yhi), cube.wcs)
+    # Round for cutout
+    yp_lo_idx = int(np.floor(yp_lo))
+    yp_hi_idx = int(np.ceil(yp_hi))
+    xp_lo_idx = int(np.floor(xp_hi))
+    xp_hi_idx = int(np.ceil(xp_lo))
 
-            # Round for cutout
-            yp_lo_idx = int(np.floor(yp_lo))
-            yp_hi_idx = int(np.ceil(yp_hi))
-            xp_lo_idx = int(np.floor(xp_hi))
-            xp_hi_idx = int(np.ceil(xp_lo))
-
-            # Use subcube for header transformation
-            cutout_cube = cube[:, yp_lo_idx:yp_hi_idx, xp_lo_idx:xp_hi_idx]
-            new_header = cutout_cube.header
-            del cube, cutout_cube
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", AstropyWarning)
-                with fits.open(
-                    cutout_args.image, memmap=True, mode="denywrite"
-                ) as hdulist:
-                    data = hdulist[0].data
-                    old_header = hdulist[0].header
-
-                    # Use numpy to force into memory
-                    sub_data = np.array(
-                        data[
-                            :,
-                            :,
-                            yp_lo_idx:yp_hi_idx,
-                            xp_lo_idx:xp_hi_idx,  # freq, Stokes, y, x
-                        ]
-                    )
-                fixed_header = fix_header(new_header, old_header)
-                # Add source name to header for CASDA
-                fixed_header["OBJECT"] = cutout_args.source_id
-                if not dryrun:
-                    fits.writeto(
-                        outfile,
-                        sub_data,
-                        header=fixed_header,
-                        overwrite=True,
-                        output_verify="fix",
-                    )
-                    logger.info(f"Written to {outfile}")
-
-        # Update database
-        myquery = {"Source_ID": cutout_args.source_id}
-
-        filename = os.path.join(
-            os.path.basename(os.path.dirname(outfile)), os.path.basename(outfile)
+    # Use subcube for header transformation
+    cutout_cube = cube[:, yp_lo_idx:yp_hi_idx, xp_lo_idx:xp_hi_idx]
+    new_header = cutout_cube.header
+    sub_data = data_in_mem[
+        :,
+        :,
+        yp_lo_idx:yp_hi_idx,
+        xp_lo_idx:xp_hi_idx,  # freq, Stokes, y, x
+    ]
+    fixed_header = fix_header(new_header, cube.header)
+    # Add source name to header for CASDA
+    fixed_header["OBJECT"] = source_id
+    if not dryrun:
+        fits.writeto(
+            outfile,
+            sub_data,
+            header=fixed_header,
+            overwrite=True,
+            output_verify="fix",
         )
-        newvalues = {
-            "$set": {
-                f"beams.{field}.{cutout_args.stoke}_beam{cutout_args.beam}_{imtype}_file": filename
-            }
-        }
+        logger.info(f"Written to {outfile}")
 
-        ret += [pymongo.UpdateOne(myquery, newvalues, upsert=True)]
+    # Update database
+    myquery = {"Source_ID": source_id}
 
-    return ret
+    filename = os.path.join(
+        os.path.basename(os.path.dirname(outfile)), os.path.basename(outfile)
+    )
+    newvalues = {"$set": {f"beams.{field}.{stoke}_beam{beam_num}_image_file": filename}}
+
+    return pymongo.UpdateOne(myquery, newvalues, upsert=True)
 
 
-@task(name="Get cutout arguments")
 def get_args(
-    island: Dict,
     comps: List[Dict],
     beam: Dict,
     island_id: str,
     outdir: str,
-    field: str,
-    datadir: str,
-    stokeslist: List[str],
-    verbose=True,
 ) -> Union[List[CutoutArgs], None]:
     """Get arguments for cutout function
 
     Args:
-        island (str): Mongo entry for RACS island
         comps (List[Dict]): List of mongo entries for RACS components in island
         beam (Dict): Mongo entry for the RACS beam
         island_id (str): RACS island ID
@@ -223,22 +196,19 @@ def get_args(
 
     logger.setLevel(logging.INFO)
 
-    assert island["Source_ID"] == island_id
     assert beam["Source_ID"] == island_id
 
     if len(comps) == 0:
         logger.warning(f"Skipping island {island_id} -- no components found")
         return None
 
-    beam_list = list(set(beam["beams"][field]["beam_list"]))
-
-    outdir = f"{outdir}/{island['Source_ID']}"
-    try_mkdir(outdir, verbose=verbose)
+    outdir = f"{outdir}/{island_id}"
+    try_mkdir(outdir)
 
     # Find image size
-    ras = []  # type: List[float]
-    decs = []  # type: List[float]
-    majs = []  # type: List[float]
+    ras: List[float] = []
+    decs: List[float] = []
+    majs: List[float] = []
     for comp in comps:
         ras = ras + [comp["RA"]]
         decs = decs + [comp["Dec"]]
@@ -273,33 +243,13 @@ def get_args(
         logger.debug(f"comps are {comps=}")
         raise e
 
-    args: List[CutoutArgs] = []
-    for beam_num in beam_list:
-        for stoke in stokeslist:
-            wild = f"{datadir}/image.restored.{stoke.lower()}*contcube*beam{beam_num:02}.conv.fits"
-            images = glob(wild)
-            if len(images) == 0:
-                raise Exception(f"No images found matching '{wild}'")
-            elif len(images) > 1:
-                raise Exception(
-                    f"More than one image found matching '{wild}'. Files {images=}"
-                )
-
-            for image in images:
-                args.append(
-                    CutoutArgs(
-                        image=image,
-                        source_id=island["Source_ID"],
-                        ra_high=ra_high.deg,
-                        ra_low=ra_low.deg,
-                        dec_high=dec_high.deg,
-                        dec_low=dec_low.deg,
-                        outdir=outdir,
-                        beam=beam_num,
-                        stoke=stoke.lower(),
-                    )
-                )
-    return args
+    return CutoutArgs(
+        ra_high=ra_high.deg,
+        ra_low=ra_low.deg,
+        dec_high=dec_high.deg,
+        dec_low=dec_low.deg,
+        outdir=outdir,
+    )
 
 
 @task(name="Find components")
@@ -340,6 +290,90 @@ def unpack(list_sq: List[Union[List[T], None, T]]) -> List[T]:
             list_fl.append(i)
     logger.debug(f"{list_fl=}")
     return list_fl
+
+
+@task(name="Cutout from big cube")
+def big_cutout(
+    beam_num: int,
+    stoke: str,
+    datadir: str,
+    host: str,
+    epoch: int,
+    field: str,
+    pad: float = 3,
+    username: Optional[str] = None,
+    password: Optional[str] = None,
+    limit: Optional[int] = None,
+) -> List[pymongo.UpdateOne]:
+    wild = (
+        f"{datadir}/image.restored.{stoke.lower()}*contcube*beam{beam_num:02}.conv.fits"
+    )
+    images = glob(wild)
+    if len(images) == 0:
+        raise Exception(f"No images found matching '{wild}'")
+    elif len(images) > 1:
+        raise Exception(f"More than one image found matching '{wild}'. Files {images=}")
+
+    image_name = images[0]
+
+    # Read the whole lad into memory
+    logger.info(f"Reading {image_name}")
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", AstropyWarning)
+        cube = SpectralCube.read(image_name, memmap=True, mode="denywrite")
+
+    data_in_mem = np.array(cube.unmasked_data[:])
+
+    beams_col, island_col, comp_col = get_db(
+        host=host, epoch=epoch, username=username, password=password
+    )
+
+    query = {
+        "$and": [
+            {f"beams.{field}": {"$exists": True}},
+            {f"beams.{field}.beam_list": beam_num},
+        ]
+    }
+    beams = list(beams_col.find(query).sort("Source_ID"))
+
+    if limit is not None:
+        logger.critical(f"Limiting to {limit} islands")
+        beams = beams[:limit]
+
+    updates: List[pymongo.UpdateOne] = []
+    for beam in beams:
+        comps = list(comp_col.find({"Source_ID": beam["Source_ID"]}))
+        cut_args = get_args(
+            comps=comps,
+            beam=beam,
+            island_id=beam["Source_ID"],
+            outdir=datadir,
+            field=field,
+            datadir=datadir,
+        )
+        image_update = cutout_image(
+            image_name=image_name,
+            data_in_mem=data_in_mem,
+            cube=cube,
+            source_id=beam["Source_ID"],
+            cutout_args=cut_args,
+            field=field,
+            beam_num=beam_num,
+            stoke=stoke,
+            pad=pad,
+            dryrun=False,
+        )
+        weight_update = cutout_weight(
+            image_name=image_name,
+            source_id=beam["Source_ID"],
+            cutout_args=cut_args,
+            field=field,
+            dryrun=False,
+        )
+        updates.append(image_update)
+        updates.append(weight_update)
+
+    return updates
 
 
 @flow(name="Cutout islands")
@@ -389,53 +423,26 @@ def cutout_islands(
 
     # Query the DB
     query = {"$and": [{f"beams.{field}": {"$exists": True}}]}
-
-    beams = list(beams_col.find(query).sort("Source_ID"))
-
-    island_ids = sorted(beams_col.distinct("Source_ID", query))
-    islands = list(
-        island_col.find({"Source_ID": {"$in": island_ids}}).sort("Source_ID")
+    unique_beams_nums: Set[int] = set(
+        beams_col.distinct(f"beams.{field}.beam_list", query)
     )
-
-    big_comps = list(comp_col.find({"Source_ID": {"$in": island_ids}}))
-    comps = []
-    for island_id in island_ids:
-        _comps = []
-        for c in big_comps:
-            if c["Source_ID"] == island_id:
-                _comps.append(c)
-        comps.append(_comps)
-
     # Create output dir if it doesn't exist
     try_mkdir(outdir)
-
-    if limit is not None:
-        logger.critical(f"Limiting to {limit} islands")
-        islands = islands[:limit]
-        island_ids = island_ids[:limit]
-        comps = comps[:limit]
-        beams = beams[:limit]
-
-    args = get_args.map(
-        island=islands,
-        comps=comps,
-        beam=beams,
-        island_id=island_ids,
-        outdir=unmapped(outdir),
-        field=unmapped(field),
-        datadir=unmapped(directory),
-        stokeslist=unmapped(stokeslist),
-        verbose=unmapped(verbose_worker),
-    )
-    # args = [a.result() for a in args]
-    # flat_args = unpack.map(args)
-    flat_args = unpack.submit(args)
-    cuts = cutout.map(
-        cutout_args=flat_args,
-        field=unmapped(field),
-        pad=unmapped(pad),
-        dryrun=unmapped(dryrun),
-    )
+    cuts: List[List[pymongo.UpdateOne]] = []
+    for stoke in stokeslist:
+        results = big_cutout.map(
+            beam_num=unique_beams_nums,
+            stoke=stoke,
+            datadir=directory,
+            host=host,
+            epoch=epoch,
+            field=field,
+            pad=pad,
+            username=username,
+            password=password,
+            limit=limit,
+        )
+        cuts.append(results)
 
     if not dryrun:
         _updates = [f.result() for f in cuts]
@@ -581,7 +588,7 @@ def cli() -> None:
         password=args.password,
     )
 
-    main(args, verbose=verbose)
+    main(args)
 
 
 if __name__ == "__main__":
