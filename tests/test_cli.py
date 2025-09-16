7f91a686b9c70ac61f88078da653b08fda1971f6
Alec Thomson <alec.thomson@skao.int>
2025-09-16 16:10:26 +0800

    Fix tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,21 +2,29 @@
 
 from __future__ import annotations
 
-import subprocess
-import unittest
+import subprocess as sp
 
 
-class test_cli(unittest.TestCase):
-    def test_cli_init(self):
-        """Tests that the CLI `spice_init` runs."""
-        res = subprocess.run(["spice_init", "--help"], check=True)
-        self.assertEqual(res.returncode, 0)
-
-    def test_cli_process(self):
-        """Tests that the CLI `spice_process` runs."""
-        res = subprocess.run(["spice_process", "--help"], check=True)
-        self.assertEqual(res.returncode, 0)
+def run_cli_help(cmd: str) -> bool:
+    res = sp.run([cmd, "--help"], check=True)
+    return res.returncode == 0
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_cli_init() -> None:
+    assert run_cli_help("spice_init")
+
+
+def test_cli_process() -> None:
+    assert run_cli_help("spice_process")
+
+
+def test_cli_region() -> None:
+    assert run_cli_help("spice_region")
+
+
+def test_cli_cat() -> None:
+    assert run_cli_help("spice_cat")
+
+
+def test_cli_image() -> None:
+    assert run_cli_help("spice_image")
